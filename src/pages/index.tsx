7810b3169dc98950daf4e5e9b0c1e27d69f992cd
Jake Cheek <jake@f-m.fm>
2022-07-24 18:51:09 -0400

    set up tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
             href='https://github.com/vercel/next.js/tree/canary/examples'
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2 className='text-red-800'>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
