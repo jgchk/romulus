272f94608ccd6a52fb885249c1cc696c285c3fb0
Jake Cheek <jake@f-m.fm>
2022-07-24 18:43:56 -0400

    set up linting

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:unicorn/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-useless-undefined': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-array-callback-reference': 'off',
+  },
+}
