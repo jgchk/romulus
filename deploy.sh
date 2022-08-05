490101bbdac28c002ef27eed1a700813ee680ff6
Jake Cheek <jake@f-m.fm>
2022-08-05 18:24:00 -0400

    remove next and nginx from docker for now

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-source .env.local
-
-# yarn build
-
-# scp -i "ssh-key.pem" -r . ${EC2_USER}@${EC2_URL}:${EC2_DEST}
-rsync -avz -e "ssh -i ssh-key.pem" -r . ${EC2_USER}@${EC2_URL}:${EC2_DEST}
