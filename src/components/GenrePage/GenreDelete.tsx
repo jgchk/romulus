2dade07ae7faaf0a1e1566f9df210f8999d08336
Jake Cheek <jake@f-m.fm>
2022-08-07 11:33:12 -0400

    extract common view from GenreDelete & GenreView pages

--- src/components/GenrePage/GenreDelete.tsx
+++ src/components/GenrePage/GenreDelete.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC, useCallback } from 'react'
 import toast from 'react-hot-toast'
-import ReactMarkdown from 'react-markdown'
 
 import { DefaultGenre } from '../../server/db/genre'
 import { useSession } from '../../services/auth'
 import { useDeleteGenreMutation, useGenreQuery } from '../../services/genres'
 import { ButtonPrimary, ButtonTertiary } from '../common/Button'
+import GenreViewData from './GenreViewData'
 
 export const GenreDelete: FC<{
   id: number
@@ -46,98 +45,7 @@ const HasData: FC<{ genre: DefaultGenre }> = ({ genre }) => {
 
   return (
     <div className='flex flex-col h-full'>
-      <div className='flex-1 overflow-auto p-4'>
-        <div className='text-lg font-bold text-gray-600 mb-4'>{genre.name}</div>
-        <div className='space-y-3'>
-          {genre.influencedByGenres.length > 0 && (
-            <div>
-              <label
-                className='block text-gray-700 text-sm'
-                htmlFor='influences'
-              >
-                Influences
-              </label>
-              <ul id='influences' className='comma-list'>
-                {genre.influencedByGenres.map(({ id, name }) => (
-                  <li key={id}>
-                    <Link href={`/genres/${id}`}>
-                      <a className='text-blue-500 hover:underline'>{name}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='short-description'
-            >
-              Short Description
-            </label>
-            <div id='short-description'>
-              {genre.shortDescription || (
-                <span>
-                  Missing a short description.{' '}
-                  {session.isLoggedIn && (
-                    <button
-                      className='text-blue-500 hover:underline'
-                      onClick={() => router.push(`/genres/${genre.id}/edit`)}
-                    >
-                      Add one.
-                    </button>
-                  )}
-                </span>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='long-description'
-            >
-              Long Description
-            </label>
-            <div id='long-description'>
-              {genre.longDescription ? (
-                <div className='prose prose-gray'>
-                  <ReactMarkdown>{genre.longDescription}</ReactMarkdown>
-                </div>
-              ) : (
-                <span>
-                  Missing a long description.{' '}
-                  {session.isLoggedIn && (
-                    <button
-                      className='text-blue-500 hover:underline'
-                      onClick={() => router.push(`/genres/${genre.id}/edit`)}
-                    >
-                      Add one.
-                    </button>
-                  )}
-                </span>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='contributors'
-            >
-              Contributors
-            </label>
-            <div id='contributors'>
-              {genre.contributors.length > 0
-                ? genre.contributors
-                    .map((contributor) => contributor.username)
-                    .join(', ')
-                : 'No contributors'}
-            </div>
-          </div>
-        </div>
-      </div>
+      <GenreViewData genre={genre} />
 
       {session.isLoggedIn && (
         <div className='border-t'>
--- src/components/GenrePage/GenreView.tsx
+++ src/components/GenrePage/GenreView.tsx
@@ -1,12 +1,11 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
-import ReactMarkdown from 'react-markdown'
 
 import { DefaultGenre } from '../../server/db/genre'
 import { useSession } from '../../services/auth'
 import { useGenreQuery } from '../../services/genres'
 import { ButtonPrimary, ButtonTertiary } from '../common/Button'
+import GenreViewData from './GenreViewData'
 
 export const GenreView: FC<{
   id: number
@@ -30,108 +29,7 @@ const HasData: FC<{ genre: DefaultGenre }> = ({ genre }) => {
 
   return (
     <div className='flex flex-col h-full'>
-      <div className='flex-1 overflow-auto p-4'>
-        <div className='text-lg font-bold text-gray-600 mb-4'>{genre.name}</div>
-        <div className='space-y-3'>
-          {genre.influencedByGenres.length > 0 && (
-            <div>
-              <label
-                className='block text-gray-700 text-sm'
-                htmlFor='influences'
-              >
-                Influences
-              </label>
-              <ul id='influences' className='comma-list'>
-                {genre.influencedByGenres.map(({ id, name }) => (
-                  <li key={id}>
-                    <Link href={`/genres/${id}`}>
-                      <a className='text-blue-500 hover:underline'>{name}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='short-description'
-            >
-              Short Description
-            </label>
-            <div id='short-description'>
-              {genre.shortDescription || (
-                <span>
-                  Missing a short description.{' '}
-                  {session.isLoggedIn && (
-                    <button
-                      className='text-blue-500 hover:underline'
-                      onClick={() =>
-                        router.push({
-                          pathname: `/genres/${genre.id}/edit`,
-                          query: { autoFocus: 'shortDescription' },
-                        })
-                      }
-                    >
-                      Add one.
-                    </button>
-                  )}
-                </span>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='long-description'
-            >
-              Long Description
-            </label>
-            <div id='long-description'>
-              {genre.longDescription ? (
-                <div className='prose prose-gray'>
-                  <ReactMarkdown>{genre.longDescription}</ReactMarkdown>
-                </div>
-              ) : (
-                <span>
-                  Missing a long description.{' '}
-                  {session.isLoggedIn && (
-                    <button
-                      className='text-blue-500 hover:underline'
-                      onClick={() =>
-                        router.push({
-                          pathname: `/genres/${genre.id}/edit`,
-                          query: { autoFocus: 'longDescription' },
-                        })
-                      }
-                    >
-                      Add one.
-                    </button>
-                  )}
-                </span>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              className='block text-gray-700 text-sm'
-              htmlFor='contributors'
-            >
-              Contributors
-            </label>
-            <div id='contributors'>
-              {genre.contributors.length > 0
-                ? genre.contributors
-                    .map((contributor) => contributor.username)
-                    .join(', ')
-                : 'No contributors'}
-            </div>
-          </div>
-        </div>
-      </div>
+      <GenreViewData genre={genre} />
 
       {session.isLoggedIn && (
         <div className='flex p-1 space-x-1 border-t'>
