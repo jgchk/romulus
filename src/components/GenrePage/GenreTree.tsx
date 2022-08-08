06a8fa5c2744f7cabec9729de34e5d7575a86a37
Jake Cheek <jake@f-m.fm>
2022-08-08 13:48:13 -0400

    add genre tree filter

--- src/components/GenrePage/GenreTree.tsx
+++ src/components/GenrePage/GenreTree.tsx
@@ -1,3 +1,4 @@
+import anyAscii from 'any-ascii'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -53,25 +54,69 @@ const Tree: FC<{ genres: DefaultGenre[]; selectedId?: number }> = ({
   genres: unsortedGenres,
   selectedId,
 }) => {
-  const genres = useMemo(
-    () => unsortedGenres.sort((a, b) => a.name.localeCompare(b.name)),
-    [unsortedGenres]
+  const [expanded, setExpanded] = useState<
+    Record<number, 'expanded' | 'collapsed' | undefined>
+  >({})
+  const [filter, setFilter] = useState('')
+
+  const genreMap = useGenreMap(unsortedGenres)
+
+  const getDescendants = useCallback(
+    (id: number) => {
+      const descendants: number[] = []
+      const queue = [id]
+
+      while (queue.length > 0) {
+        const currId = queue.shift()
+        if (currId === undefined) break
+
+        const currGenre = genreMap[currId]
+        const childIds = currGenre.childGenres.map((g) => g.id)
+        descendants.push(...childIds)
+        queue.push(...childIds)
+      }
+
+      return descendants
+    },
+    [genreMap]
   )
+
+  const getMatchesFilter = useCallback(
+    (id: number) => {
+      if (!filter) return false
+
+      const genre = genreMap[id]
+      return anyAscii(genre.name.toLowerCase()).includes(
+        anyAscii(filter.toLowerCase())
+      )
+    },
+    [filter, genreMap]
+  )
+
+  const genres = useMemo(() => {
+    let gs = unsortedGenres
+    if (filter) {
+      gs = gs.filter((g) => {
+        const descendants = getDescendants(g.id)
+        return [g.id, ...descendants].some((id) => getMatchesFilter(id))
+      })
+    }
+    return gs.sort((a, b) => a.name.localeCompare(b.name))
+  }, [filter, getDescendants, getMatchesFilter, unsortedGenres])
 
   const topLevelGenres = useMemo(
     () => genres.filter((genre) => genre.parentGenres.length === 0),
     [genres]
   )
 
-  const genreMap = useGenreMap(genres)
-
   const session = useSession()
-
-  const [expanded, setExpanded] = useState<number[]>([])
 
   const renderGenre = useCallback(
     (genre: DefaultGenre) => {
-      const isExpanded = expanded.includes(genre.id)
+      const isExpanded =
+        expanded[genre.id] === 'expanded' ||
+        (expanded[genre.id] === undefined &&
+          getDescendants(genre.id).some((id) => getMatchesFilter(id)))
 
       return (
         <li
@@ -88,19 +133,14 @@ const Tree: FC<{ genres: DefaultGenre[]; selectedId?: number }> = ({
                 'p-1 hover:bg-blue-100 hover:text-blue-600 rounded-sm text-gray-500',
                 genre.childGenres.length === 0 && 'invisible'
               )}
-              onClick={() => {
-                if (isExpanded) {
-                  setExpanded(expanded.filter((id) => id !== genre.id))
-                } else {
-                  setExpanded([...expanded, genre.id])
-                }
-              }}
+              onClick={() =>
+                setExpanded({
+                  ...expanded,
+                  [genre.id]: isExpanded ? 'collapsed' : 'expanded',
+                })
+              }
             >
-              {expanded.includes(genre.id) ? (
-                <RiArrowDownSLine />
-              ) : (
-                <RiArrowRightSLine />
-              )}
+              {isExpanded ? <RiArrowDownSLine /> : <RiArrowRightSLine />}
             </button>
             <Link
               href={{
@@ -127,27 +167,31 @@ const Tree: FC<{ genres: DefaultGenre[]; selectedId?: number }> = ({
         </li>
       )
     },
-    [expanded, genreMap, selectedId]
+    [expanded, genreMap, getDescendants, getMatchesFilter, selectedId]
   )
 
-  if (genres.length === 0) {
-    return (
-      <div className='w-full h-full flex flex-col items-center justify-center text-gray-400'>
-        <div>No genres found.</div>
-        {session.isLoggedIn && (
-          <div>
-            <Link href={{ pathname: '/genres/create' }}>
-              <a className='text-blue-500 hover:underline'>Create one.</a>
-            </Link>
-          </div>
-        )}
-      </div>
-    )
-  }
-
   return (
-    <div className='w-full h-full'>
-      <ul>{topLevelGenres.map((genre) => renderGenre(genre))}</ul>
+    <div className='w-full h-full flex flex-col'>
+      <input
+        className='border rounded-sm p-1 px-2 mb-2 w-full'
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        placeholder='Filter...'
+      />
+      {genres.length > 0 ? (
+        <ul>{topLevelGenres.map((genre) => renderGenre(genre))}</ul>
+      ) : (
+        <div className='flex-1 w-full flex flex-col items-center justify-center text-gray-400'>
+          <div>No genres found.</div>
+          {session.isLoggedIn && (
+            <div>
+              <Link href={{ pathname: '/genres/create' }}>
+                <a className='text-blue-500 hover:underline'>Create one.</a>
+              </Link>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
