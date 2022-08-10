59ff74e832b44ea1c3541d1e999c029f847091c0
Jake Cheek <jake@f-m.fm>
2022-08-10 18:59:28 -0400

    add unsaved changes warning

--- src/components/GenrePage/GenreForm.tsx
+++ src/components/GenrePage/GenreForm.tsx
@@ -1,6 +1,7 @@
 import { GenreType, Permission } from '@prisma/client'
 import clsx from 'clsx'
-import { FC, useCallback, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { FC, useCallback, useEffect, useMemo, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
 import { DefaultGenre } from '../../server/db/genre/types'
@@ -49,7 +50,7 @@ const GenreForm: FC<{
     control,
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, dirtyFields },
     setFocus,
   } = useForm<GenreFormFields>({
     defaultValues: {
@@ -90,6 +91,38 @@ const GenreForm: FC<{
   )
 
   useEffect(() => setFocus(autoFocus ?? 'name'), [autoFocus, setFocus])
+
+  const router = useRouter()
+  const hasDirtyFields = useMemo(
+    () => Object.values(dirtyFields).some((dirty) => !!dirty),
+    [dirtyFields]
+  )
+  useEffect(() => {
+    const confirmationMessage = 'Changes you made may not be saved.'
+    const beforeUnloadHandler = (e: BeforeUnloadEvent) => {
+      ;(e || window.event).returnValue = confirmationMessage
+      return confirmationMessage // Gecko + Webkit, Safari, Chrome etc.
+    }
+    const beforeRouteHandler = (url: string) => {
+      if (router.pathname !== url && !confirm(confirmationMessage)) {
+        // to inform NProgress or something ...
+        router.events.emit('routeChangeError')
+        // tslint:disable-next-line: no-string-throw
+        throw `Route change to "${url}" was aborted (this error can be safely ignored). See https://github.com/zeit/next.js/issues/2476.`
+      }
+    }
+    if (hasDirtyFields) {
+      window.addEventListener('beforeunload', beforeUnloadHandler)
+      router.events.on('routeChangeStart', beforeRouteHandler)
+    } else {
+      window.removeEventListener('beforeunload', beforeUnloadHandler)
+      router.events.off('routeChangeStart', beforeRouteHandler)
+    }
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnloadHandler)
+      router.events.off('routeChangeStart', beforeRouteHandler)
+    }
+  }, [hasDirtyFields, router.events, router.pathname])
 
   return (
     <form
