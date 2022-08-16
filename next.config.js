76320a6c81c2c1de8d949e6228b91ca9af5ba817
Jake Cheek <jake@f-m.fm>
2022-08-15 22:53:29 -0400

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { env } = require('./src/server/env')
 
 const { withRoutes } = require('nextjs-routes/next-config.cjs')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -18,19 +22,21 @@ function getConfig(config) {
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
-module.exports = withRoutes(
-  getConfig({
-    output: 'standalone',
-    reactStrictMode: true,
-    swcMinify: true,
+module.exports = withBundleAnalyzer(
+  withRoutes(
+    getConfig({
+      output: 'standalone',
+      reactStrictMode: true,
+      swcMinify: true,
 
-    /**
-     * Dynamic configuration available for the browser and server.
-     * Note: requires `ssr: true` or a `getInitialProps` in `_app.tsx`
-     * @link https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
-     */
-    publicRuntimeConfig: {
-      NODE_ENV: env.NODE_ENV,
-    },
-  })
+      /**
+       * Dynamic configuration available for the browser and server.
+       * Note: requires `ssr: true` or a `getInitialProps` in `_app.tsx`
+       * @link https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
+       */
+      publicRuntimeConfig: {
+        NODE_ENV: env.NODE_ENV,
+      },
+    })
+  )
 )
