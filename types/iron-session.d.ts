ea295c97bdd6f033d3092b26a7cc1db845f35e75
Jake Cheek <jake@f-m.fm>
2022-08-27 19:56:24 -0400

    update iron-session type

--- types/iron-session.d.ts
+++ types/iron-session.d.ts
@@ -2,6 +2,6 @@ import { DefaultAccount } from '../src/server/db/account/outputs'
 
 declare module 'iron-session' {
   interface IronSessionData {
-    account: DefaultAccount
+    account?: DefaultAccount
   }
 }
