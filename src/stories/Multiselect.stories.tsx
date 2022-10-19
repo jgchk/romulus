85693d3615cd24832696cdb1375f02c810d30949
Jake Cheek <jake@f-m.fm>
2022-10-19 15:10:43 -0400

    update Select and Multiselect stories

--- src/stories/Multiselect.stories.tsx
+++ src/stories/Multiselect.stories.tsx
@@ -37,6 +37,7 @@ const Template: Story<ComponentProps<typeof M> & { numOptions: number }> = ({
       query={query}
       onQueryChange={setQuery}
       options={options}
+      className='w-full'
     >
       <M.Box>
         {selected.map((item) => (
@@ -66,5 +67,68 @@ const Template: Story<ComponentProps<typeof M> & { numOptions: number }> = ({
 
 export const Multiselect = Template.bind({})
 Multiselect.args = {
+  numOptions: 100,
+}
+
+const PopoverDirectionTemplate: Story<
+  ComponentProps<typeof M> & { numOptions: number }
+> = ({ numOptions, ...args }) => {
+  const all: HasId[] = range(1, 100).map((id) => ({ id }))
+
+  const [selected, setSelected] = useState<HasId[]>([])
+  const [query, setQuery] = useState('')
+
+  const options = useMemo(() => {
+    const selectedIds = new Set(selected.map((s) => s.id))
+    return all.filter(
+      (i) => i.id.toString().startsWith(query) && !selectedIds.has(i.id)
+    )
+  }, [all, query, selected])
+
+  const [page, setPage] = useState(1)
+
+  return (
+    <div className='flex h-[calc(100vh-32px)] w-full items-end'>
+      <M
+        {...args}
+        value={selected}
+        onChange={setSelected}
+        query={query}
+        onQueryChange={setQuery}
+        options={options}
+        className='flex-1'
+      >
+        <M.Box>
+          {selected.map((item) => (
+            <M.Selected key={item.id} item={item}>
+              {item.id}
+            </M.Selected>
+          ))}
+          <M.Input />
+        </M.Box>
+        <M.Options>
+          {options.slice(0, page * numOptions).map((option) => (
+            <M.Option key={option.id} item={option}>
+              {option.id}
+            </M.Option>
+          ))}
+          {options.length > page * numOptions && (
+            <div className='flex w-full justify-center'>
+              <Button
+                template='secondary'
+                onClick={() => setPage((p) => p + 1)}
+              >
+                Load More
+              </Button>
+            </div>
+          )}
+        </M.Options>
+      </M>
+    </div>
+  )
+}
+
+export const PopoverDirection = PopoverDirectionTemplate.bind({})
+PopoverDirection.args = {
   numOptions: 100,
 }
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -9,12 +9,12 @@ export default {
   component: SelectComponent,
 } as ComponentMeta<typeof SelectComponent>
 
-const Template: ComponentStory<typeof SelectComponent> = (args) => {
+const SelectTemplate: ComponentStory<typeof SelectComponent> = (args) => {
   const [value, setValue] = useState<Option>()
   return <SelectComponent {...args} value={value} onChange={setValue} />
 }
 
-export const Select = Template.bind({})
+export const Select = SelectTemplate.bind({})
 Select.args = {
   options: range(1, 10).map((v) => ({ id: v, label: v })),
   placeholder: 'Input',
@@ -23,4 +23,26 @@ Select.args = {
 Select.argTypes = {
   value: { control: null, table: { disable: true } },
   onChange: { control: null, table: { disable: true } },
+}
+
+const PopoverDirectionTemplate: ComponentStory<typeof SelectComponent> = (
+  args
+) => {
+  const [value, setValue] = useState<Option>()
+  return (
+    <div className='flex h-[calc(100vh-32px)] w-full items-end'>
+      <SelectComponent
+        {...args}
+        value={value}
+        onChange={setValue}
+        className='flex-1'
+      />
+    </div>
+  )
+}
+
+export const PopoverDirection = PopoverDirectionTemplate.bind({})
+PopoverDirection.args = {
+  options: range(1, 10).map((v) => ({ id: v, label: v })),
+  placeholder: 'Input',
 }
