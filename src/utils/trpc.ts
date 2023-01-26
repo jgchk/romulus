616afdda380561da604ea11f8dd6fd2110eed8fa
Jake Cheek <jake@f-m.fm>
2023-01-25 20:57:23 -0500

    remove loggerLink

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,7 +8,6 @@ import { createTRPCNext } from '@trpc/next'
 import {
   createTRPCReact,
   httpBatchLink,
-  loggerLink,
   TRPCClientErrorLike,
 } from '@trpc/react-query'
 import type { inferProcedureInput, inferProcedureOutput } from '@trpc/server'
@@ -73,12 +72,6 @@ export const trpcNext = createTRPCNext<AppRouter, SSRContext>({
       cookie: ctx?.req?.headers.cookie,
     },
     links: [
-      // adds pretty logs to your console in development and logs errors in production
-      loggerLink({
-        enabled: (opts) =>
-          process.env.NODE_ENV === 'development' ||
-          (opts.direction === 'down' && opts.result instanceof Error),
-      }),
       httpBatchLink({
         url: `${getBaseTrpcUrl()}/api/trpc`,
         headers() {
