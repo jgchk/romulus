7176655aa9c48dac89dedd3cc5d016045fc7e60c
Jake Cheek <jake@f-m.fm>
2024-05-19 17:30:30 -0400

    create initial account page

---
+++ src/routes/accounts/[id]/+page.server.ts
@@ -0,0 +1,44 @@
+import { error } from '@sveltejs/kit'
+import { z } from 'zod'
+
+import { db } from '$lib/server/db'
+
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ params }) => {
+  const maybeId = z.coerce.number().int().safeParse(params.id)
+  if (!maybeId.success) {
+    return error(400, { message: 'Invalid genre ID' })
+  }
+  const id = maybeId.data
+
+  const maybeAccount = await db.query.accounts.findFirst({
+    where: (accounts, { eq }) => eq(accounts.id, id),
+  })
+
+  if (!maybeAccount) {
+    return error(404, 'Account not found')
+  }
+
+  const account = maybeAccount
+
+  const history = await db.query.genreHistory.findMany({
+    where: (genreHistory, { eq }) => eq(genreHistory.accountId, id),
+    columns: {
+      treeGenreId: true,
+      operation: true,
+    },
+  })
+
+  const numCreated = new Set(
+    history.filter((h) => h.operation === 'CREATE').map((h) => h.treeGenreId),
+  ).size
+  const numUpdated = new Set(
+    history.filter((h) => h.operation === 'UPDATE').map((h) => h.treeGenreId),
+  ).size
+  const numDeleted = new Set(
+    history.filter((h) => h.operation === 'DELETE').map((h) => h.treeGenreId),
+  ).size
+
+  return { account, numCreated, numUpdated, numDeleted }
+}
---
+++ src/routes/accounts/[id]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Card from '$lib/atoms/Card.svelte'
+
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<div class="h-full w-full">
+  <Card class="h-full p-4">
+    <div class="flex h-full max-h-full min-h-0 flex-col">
+      <div class="text-xl font-bold">{data.account.username}</div>
+
+      <div class="py-2">
+        <div>Genres created: {data.numCreated}</div>
+        <div>Genres edited: {data.numUpdated}</div>
+        <div>Genres deleted: {data.numDeleted}</div>
+      </div>
+
+      <div class="min-h-0 flex-1 overflow-auto">TODO</div>
+    </div>
+  </Card>
+</div>
