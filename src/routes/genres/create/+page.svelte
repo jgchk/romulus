3c9ab03664728792adad96af8ac8c92cec020ea1
Jake Cheek <jake.cheek@itential.com>
2024-06-19 13:59:20 -0400

    deselect genre in tree when creating a new one

--- src/routes/genres/create/+page.svelte
+++ src/routes/genres/create/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import GenreForm from '../GenreForm.svelte'
+  import { treeState } from '../GenreNavigator/GenreTree/state'
   import type { PageData } from './$types'
 
   export let data: PageData
+
+  treeState.setSelectedId(undefined)
+  treeState.setSelectedPath(undefined)
 </script>
 
 <GenreForm data={data.form} genres={data.streamed.genres} />
