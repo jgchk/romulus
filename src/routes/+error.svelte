222aaf1a3ac1289132dd30f805692ab2e396e61a
Jake Cheek <jake.cheek@itential.com>
2024-06-24 16:03:58 -0400

    add fancy error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<div class="flex h-full max-h-96 w-full items-center justify-center">
+  <div class="flex flex-col items-center">
+    <h1 class="text-lg font-bold">{$page.status}</h1>
+    <p>{$page.error?.message ?? 'An unknown error occurred'}</p>
+  </div>
+</div>
