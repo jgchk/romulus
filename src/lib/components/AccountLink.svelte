a0dd5ba52aed7e03a3ffc902a7ba9b171f2cb060
Jake Cheek <jake.cheek@itential.com>
2024-06-27 18:52:43 -0400

    remove code hover effect

--- src/lib/components/AccountLink.svelte
+++ src/lib/components/AccountLink.svelte
@@ -14,16 +14,9 @@
   class={tw(
     'transition-all',
     isSpecial
-      ? 'special bg-gradient-to-r from-code1-500 to-code2-500 bg-clip-text font-medium text-transparent'
+      ? 'bg-gradient-to-r from-code1-500 to-code2-500 bg-clip-text font-medium text-transparent'
       : 'text-primary-500 hover:underline',
     class_,
   )}
   href="/accounts/{id}"><span class="hover:underline">{username}</span></a
 >
-
-<style lang="postcss">
-  a.special:hover {
-    -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: theme(colors.code1.500);
-  }
-</style>
