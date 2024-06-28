d32c6dc43f904529158c3f15c4c904f9a6e11bba
Jake Cheek <jake.cheek@itential.com>
2024-06-28 16:05:24 -0400

    ci: disable @typescript-eslint/no-misused-promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -49,5 +49,6 @@ module.exports = {
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-misused-promises': 'off',
   },
 }
