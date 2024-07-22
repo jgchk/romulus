538ef2ab184295365efa522724484452c6adbebc
Jake Cheek <jake.cheek@itential.com>
2024-07-22 07:34:41 -0400

    feat: New, simpler Select component

---
+++ src/lib/atoms/Select2.svelte
@@ -0,0 +1,143 @@
+<script lang="ts" context="module">
+  export type T = string | number
+</script>
+
+<script lang="ts" generics="T">
+  import { flip, offset } from '@floating-ui/dom'
+  import { CaretDown } from 'phosphor-svelte'
+  import { createEventDispatcher } from 'svelte'
+  import { fade } from 'svelte/transition'
+
+  import { clickOutside } from '$lib/actions/clickOutside'
+  import { createPopoverActions } from '$lib/actions/popover'
+  import { cn } from '$lib/utils/dom'
+
+  import type { Option } from './Select2'
+
+  export let value: Option<T>['value'] | undefined = undefined
+  export let options: Option<T>[] = []
+  export let placeholder: string | undefined = 'Select...'
+  export let id: string | undefined = undefined
+
+  let class_: string | undefined = undefined
+  export { class_ as class }
+
+  $: selectedOption = options.find((option) => option.value === value)
+
+  let open = false
+  let hoveredIndex = options.findIndex((option) => option.value === value)
+  if (hoveredIndex === -1) hoveredIndex = 0
+
+  $: if (!open) {
+    hoveredIndex = options.findIndex((option) => option.value === value)
+    if (hoveredIndex === -1) hoveredIndex = 0
+  }
+
+  const dispatch = createEventDispatcher<{ change: Option<T> }>()
+  const handleSelect = (option: Option<T>) => {
+    value = option.value
+    dispatch('change', option)
+    open = false
+  }
+
+  const [popoverReference, popoverElement] = createPopoverActions({
+    middleware: [offset(4), flip()],
+  })
+</script>
+
+<div
+  data-testid="select-root"
+  class={cn('relative', class_)}
+  use:clickOutside={(e) => {
+    if (open) {
+      e.stopPropagation()
+      open = false
+    }
+  }}
+>
+  <button
+    {id}
+    class="flex w-full items-center justify-between rounded border border-gray-300 bg-black bg-opacity-[0.04] px-2 py-1.5 text-left text-sm outline-none transition hover:bg-opacity-[0.07] focus:border-secondary-500 dark:border-gray-600 dark:bg-white dark:bg-opacity-5 dark:hover:bg-opacity-10"
+    type="button"
+    on:click={() => (open = !open)}
+    on:keydown={(e) => {
+      if (e.key === 'Tab') {
+        open = false
+      } else if (e.key === 'ArrowUp') {
+        e.preventDefault()
+        if (!open) {
+          open = true
+          return
+        }
+        hoveredIndex = (hoveredIndex - 1 + options.length) % options.length
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault()
+        if (!open) {
+          open = true
+          return
+        }
+        hoveredIndex = (hoveredIndex + 1) % options.length
+      } else if (e.key === 'Enter' || e.key === ' ') {
+        e.preventDefault()
+        if (!open) {
+          open = true
+          return
+        }
+        const option = options.at(hoveredIndex)
+        if (option) {
+          handleSelect(option)
+        }
+      }
+    }}
+    use:popoverReference
+  >
+    {#if value !== undefined}
+      {#if selectedOption}
+        {selectedOption.label}
+      {:else}
+        Unknown value
+      {/if}
+    {:else}
+      <span class="text-gray-400 transition dark:text-gray-600">{placeholder}</span>
+    {/if}
+
+    <CaretDown size={14} class={cn('transition-transform', open && 'rotate-180 transform')} />
+  </button>
+
+  {#if open}
+    <ul
+      role="listbox"
+      class="relative z-10 max-h-[calc(100vh/3)] w-full overflow-auto rounded border border-gray-300 bg-gray-100 p-1 text-sm text-black shadow transition dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+      use:popoverElement
+      on:pointerdown|stopPropagation
+      transition:fade={{ duration: 75 }}
+    >
+      {#each options as option, i (option.value)}
+        <li>
+          <input
+            type="button"
+            role="option"
+            tabindex="-1"
+            class="absolute -left-[99999999px] -top-[99999999px]"
+            value={option.value}
+            aria-selected={option.value === value}
+            on:click={() => handleSelect(option)}
+            on:mouseenter={() => (hoveredIndex = i)}
+          />
+          <button
+            type="button"
+            tabindex="-1"
+            class={cn(
+              'w-full rounded border border-transparent p-1 px-1.5 text-left transition',
+              hoveredIndex === i && 'bg-gray-200 dark:bg-gray-700',
+            )}
+            on:click={() => handleSelect(option)}
+            on:mouseenter={() => (hoveredIndex = i)}
+          >
+            {option.label}
+          </button>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
--- src/routes/genres/GenreTypeSelect.svelte
+++ src/routes/genres/GenreTypeSelect.svelte
@@ -1,36 +1,15 @@
 <script lang="ts">
-  import { type ComponentProps, createEventDispatcher } from 'svelte'
+  import { type ComponentProps } from 'svelte'
 
-  import type { Option } from '$lib/atoms/Select'
-  import Select from '$lib/atoms/Select.svelte'
+  import Select from '$lib/atoms/Select2.svelte'
   import { GENRE_TYPES, type GenreType, GenreTypeNames } from '$lib/types/genres'
 
-  type Opt = Option<{ value: GenreType }>
-
-  interface $$Props extends Omit<ComponentProps<Select<Opt>>, 'value' | 'options'> {
-    value?: GenreType
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  type $$Props = Omit<ComponentProps<Select<GenreType>>, 'options'>
 
   export let value: $$Props['value'] = undefined
 
   const options = GENRE_TYPES.map((type) => ({ value: type, label: GenreTypeNames[type] }))
-
-  const valueOption = options.find((o) => o.value === value)
-
-  const dispatch = createEventDispatcher<{
-    change: { value: GenreType }
-  }>()
 </script>
 
-<Select
-  value={valueOption}
-  {options}
-  on:change={(e) => {
-    value = e.detail.value?.value
-    if (value) {
-      dispatch('change', { value })
-    }
-  }}
-  on:blur
-  {...$$restProps}
-/>
+<Select bind:value {options} on:change {...$$restProps} />
