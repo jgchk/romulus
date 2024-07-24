45bdde6ed4e34d348b87465b6582779fa21c8e55
Jake Cheek <jake.cheek@itential.com>
2024-07-24 14:02:31 -0400

    refactor: Move GenreSearchResults into separate component

--- src/routes/genres/GenreNavigator/GenreSearchResults.svelte
+++ src/routes/genres/GenreNavigator/GenreSearchResults.svelte
@@ -3,11 +3,10 @@
 
 <script lang="ts">
   import VirtualList from '$lib/atoms/VirtualList.svelte'
-  import GenreTypeChip from '$lib/components/GenreTypeChip.svelte'
   import { user } from '$lib/contexts/user'
-  import { userSettings } from '$lib/contexts/user-settings'
   import { searchGenres } from '$lib/types/genres'
 
+  import GenreSearchResult from './GenreSearchResult.svelte'
   import type { TreeGenre } from './GenreTree/state'
   import { searchStore } from './state'
 
@@ -19,28 +18,7 @@
 <div aria-label="Genre Search Results" class="h-full overflow-auto p-4">
   {#if matches.length > 0}
     <VirtualList items={matches} let:item={match}>
-      <a
-        href="/genres/{match.genre.id}"
-        class="group block truncate rounded border border-black border-opacity-0 px-1.5 text-[0.93rem] text-gray-600 transition hover:border-opacity-[0.03] hover:bg-gray-200 hover:text-black dark:border-white dark:border-opacity-0 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white"
-        on:click={() => searchStore.clearFilter()}
-      >
-        {match.genre.name}
-        {#if match.genre.subtitle}
-          {' '}
-          <span
-            class="text-[0.8rem] text-gray-500 transition group-hover:text-gray-600 dark:group-hover:text-gray-400"
-            >[{match.genre.subtitle}]</span
-          >
-        {/if}
-        {#if match.matchedAka}
-          {' '}
-          <span class="text-[0.8rem]">({match.matchedAka})</span>
-        {/if}
-        {#if $userSettings.showTypeTags && match.genre.type !== 'STYLE'}
-          {' '}
-          <GenreTypeChip type={match.genre.type} />
-        {/if}
-      </a>
+      <GenreSearchResult {match} />
     </VirtualList>
   {:else}
     <div
