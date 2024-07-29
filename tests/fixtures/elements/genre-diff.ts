fdd5111375e413d4e490ea3f379c296a35713c82
Jake Cheek <jake.cheek@itential.com>
2024-07-29 19:16:51 -0400

    test: Add full e2e test for deleting mid-level genres with parents and children

--- tests/fixtures/elements/genre-diff.ts
+++ tests/fixtures/elements/genre-diff.ts
@@ -1,4 +1,4 @@
-import type { Locator } from '@playwright/test'
+import { expect, type Locator } from '@playwright/test'
 
 export class GenreDiffEntry {
   readonly operation: Locator
@@ -20,14 +20,67 @@ export class GenreDiffEntry {
     this.account = this.element.getByTestId('genre-diff-account')
     this.time = this.element.getByTestId('genre-diff-time')
 
-    this.name = this.element.locator('.genre-diff__name')
-    this.subtitle = this.element.locator('.genre-diff__subtitle')
-    this.type = this.element.locator('.genre-diff__type')
-    this.akas = this.element.locator('.genre-diff__akas')
-    this.parents = this.element.locator('.genre-diff__parents')
-    this.influences = this.element.locator('.genre-diff__influences')
-    this.shortDescription = this.element.locator('.genre-diff__short-description')
-    this.longDescription = this.element.locator('.genre-diff__long-description')
-    this.notes = this.element.locator('.genre-diff__notes')
+    this.name = this.element.getByTestId('genre-diff-name')
+    this.subtitle = this.element.getByTestId('genre-diff-subtitle')
+    this.type = this.element.getByTestId('genre-diff-type')
+    this.akas = this.element.getByTestId('genre-diff-akas')
+    this.parents = this.element.getByTestId('genre-diff-parents')
+    this.influences = this.element.getByTestId('genre-diff-influences')
+    this.shortDescription = this.element.getByTestId('genre-diff-short-description')
+    this.longDescription = this.element.getByTestId('genre-diff-long-description')
+    this.notes = this.element.getByTestId('genre-diff-notes')
+  }
+
+  async expectData(data: {
+    operation?: string
+    account?: string
+    time?: string
+
+    name?: string
+    subtitle?: string
+    type?: string
+    akas?: string
+    parents?: string
+    influences?: string
+    shortDescription?: string
+    longDescription?: string
+    notes?: string
+  }): Promise<void> {
+    if (data.operation) {
+      await expect(this.operation).toHaveText(data.operation)
+    }
+    if (data.account) {
+      await expect(this.account).toHaveText(data.account)
+    }
+    if (data.time) {
+      await expect(this.time).toHaveText(data.time)
+    }
+    if (data.name) {
+      await expect(this.name).toHaveText(data.name)
+    }
+    if (data.subtitle) {
+      await expect(this.subtitle).toHaveText(data.subtitle)
+    }
+    if (data.type) {
+      await expect(this.type).toHaveText(data.type)
+    }
+    if (data.akas) {
+      await expect(this.akas).toHaveText(data.akas)
+    }
+    if (data.parents) {
+      await expect(this.parents).toHaveText(data.parents)
+    }
+    if (data.influences) {
+      await expect(this.influences).toHaveText(data.influences)
+    }
+    if (data.shortDescription) {
+      await expect(this.shortDescription).toHaveText(data.shortDescription)
+    }
+    if (data.longDescription) {
+      await expect(this.longDescription).toHaveText(data.longDescription)
+    }
+    if (data.notes) {
+      await expect(this.notes).toHaveText(data.notes)
+    }
   }
 }
--- tests/routes/genres/index.test.ts
+++ tests/routes/genres/index.test.ts
@@ -1,3 +1,11 @@
+import { expect } from '@playwright/test'
+
+import type { InsertAccount } from '$lib/server/db/schema'
+
+import { test } from '../../fixtures'
+import { GenreDiffEntry } from '../../fixtures/elements/genre-diff'
+import { GenreTreeGenre } from '../../fixtures/elements/genre-tree'
+import { createAccounts, createGenres, deleteAccounts, deleteGenres } from '../../utils'
 import createGenrePageTests from './create'
 import editGenrePageTests from './edit'
 import treeTests from './tree'
@@ -5,3 +13,87 @@ import treeTests from './tree'
 createGenrePageTests()
 editGenrePageTests()
 treeTests()
+
+const TEST_ACCOUNT: InsertAccount = {
+  username: 'test-username-genres-page',
+  password: 'test-password-genres-page',
+  permissions: ['EDIT_GENRES'],
+}
+
+test.describe('genres page', () => {
+  test.beforeEach(async ({ dbConnection }) => {
+    await createAccounts([TEST_ACCOUNT], dbConnection)
+  })
+
+  test.afterEach(async ({ dbConnection }) => {
+    await deleteAccounts([TEST_ACCOUNT.username], dbConnection)
+    await deleteGenres(dbConnection)
+  })
+
+  test("when the user deletes a genre, its children should be moved under the genre's parents and history entries should be created", async ({
+    dbConnection,
+    signInPage,
+    genrePage,
+    genreTree,
+    genreHistoryPage,
+  }) => {
+    const genres = await createGenres(
+      [
+        { name: 'Parent' },
+        { name: 'Child', parents: ['Parent'] },
+        { name: 'Grandchild', parents: ['Child'] },
+      ],
+      dbConnection,
+    )
+
+    const parentGenre = genres.find((genre) => genre.name === 'Parent')
+    if (!parentGenre) {
+      throw new Error('Parent genre not found')
+    }
+
+    const childGenre = genres.find((genre) => genre.name === 'Child')
+    if (!childGenre) {
+      throw new Error('Child genre not found')
+    }
+
+    const grandchildGenre = genres.find((genre) => genre.name === 'Grandchild')
+    if (!grandchildGenre) {
+      throw new Error('Grandchild genre not found')
+    }
+
+    await signInPage.goto()
+    await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
+
+    await genrePage.goto(childGenre.id)
+    await genrePage.delete()
+
+    await expect(genreTree.genres).toHaveCount(2)
+    await expect(new GenreTreeGenre(genreTree.genres.nth(0)).name).toHaveText('Parent')
+    await expect(new GenreTreeGenre(genreTree.genres.nth(1)).name).toHaveText('Grandchild')
+
+    await new GenreTreeGenre(genreTree.genres.nth(0)).collapseButton.click()
+    await expect(genreTree.genres).toHaveCount(1)
+    await expect(new GenreTreeGenre(genreTree.genres.nth(0)).name).toHaveText('Parent')
+
+    await genreHistoryPage.goto(childGenre.id)
+    await expect(genreHistoryPage.entries).toHaveCount(1)
+    await new GenreDiffEntry(genreHistoryPage.entries.nth(0)).expectData({
+      operation: 'Delete',
+      account: TEST_ACCOUNT.username,
+      name: 'Child',
+      parents: 'Parent',
+    })
+
+    await genreHistoryPage.goto(grandchildGenre.id)
+    await expect(genreHistoryPage.entries).toHaveCount(1)
+    await new GenreDiffEntry(genreHistoryPage.entries.nth(0)).expectData({
+      operation: 'Update',
+      account: TEST_ACCOUNT.username,
+      name: 'Grandchild',
+      parents: 'Parent',
+    })
+
+    await genreHistoryPage.goto(parentGenre.id)
+    await expect(genreHistoryPage.entries).toHaveCount(0)
+  })
+})
