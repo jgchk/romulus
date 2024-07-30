30df7d62b3ce21a6be1850b7ee2b87d83ede28fc
Jake Cheek <jake.cheek@itential.com>
2024-07-30 08:12:40 -0400

    refactor: inline nested e2e tests

--- tests/routes/genres/create/index.ts
+++ tests/routes/genres/create/index.ts
@@ -3,13 +3,7 @@ import { expect } from '@playwright/test'
 import { test } from '../../../fixtures'
 import { GenreDiffEntry } from '../../../fixtures/elements/genre-diff'
 import type { GenreFormData } from '../../../fixtures/elements/genre-form'
-import {
-  createAccounts,
-  type CreatedAccount,
-  createGenres,
-  deleteAccounts,
-  deleteGenres,
-} from '../../../utils'
+import { createAccounts, createGenres, deleteAccounts, deleteGenres } from '../../../utils'
 
 const TEST_ACCOUNT = {
   username: 'test-username-genres-create',
@@ -18,125 +12,154 @@ const TEST_ACCOUNT = {
 
 export default function createGenrePageTests() {
   test.describe('create', () => {
-    test.describe('when not logged in', () => {
-      test.beforeEach(async ({ createGenrePage }) => {
-        await createGenrePage.goto()
-      })
-
-      test('should show error message', async ({ errorPage }) => {
-        await errorPage.expectError('403', 'You do not have permission to create genres')
-      })
+    test.afterEach(async ({ dbConnection }) => {
+      await deleteAccounts([TEST_ACCOUNT.username], dbConnection)
+      await deleteGenres(dbConnection)
     })
 
-    test.describe('when logged in', () => {
-      test.describe('without EDIT_GENRES permission', () => {
-        test.beforeAll(async ({ dbConnection }) => {
-          await createAccounts([TEST_ACCOUNT], dbConnection)
-        })
+    test('when not logged in, should show error message', async ({
+      createGenrePage,
+      errorPage,
+    }) => {
+      await createGenrePage.goto()
+      await errorPage.expectError('403', 'You do not have permission to create genres')
+    })
 
-        test.afterAll(async ({ dbConnection }) => {
-          await deleteAccounts([TEST_ACCOUNT.username], dbConnection)
-        })
+    test('when logged in without EDIT_GENRES permission, should show error message', async ({
+      dbConnection,
+      signInPage,
+      createGenrePage,
+      errorPage,
+    }) => {
+      await createAccounts([TEST_ACCOUNT], dbConnection)
 
-        test.beforeEach(async ({ signInPage, createGenrePage }) => {
-          await signInPage.goto()
-          await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
-          await createGenrePage.goto()
-        })
+      await signInPage.goto()
+      await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
 
-        test('should show error message', async ({ errorPage }) => {
-          await errorPage.expectError('403', 'You do not have permission to create genres')
-        })
-      })
+      await createGenrePage.goto()
 
-      test.describe('with EDIT_GENRES permission', () => {
-        let account: CreatedAccount
+      await errorPage.expectError('403', 'You do not have permission to create genres')
+    })
 
-        test.beforeAll(async ({ dbConnection }) => {
-          ;[account] = await createAccounts(
-            [{ ...TEST_ACCOUNT, permissions: ['EDIT_GENRES'] }],
-            dbConnection,
-          )
-        })
+    test('when logged in with EDIT_GENRES permission, should default to Style type', async ({
+      dbConnection,
+      signInPage,
+      createGenrePage,
+    }) => {
+      const [account] = await createAccounts(
+        [{ ...TEST_ACCOUNT, permissions: ['EDIT_GENRES'] }],
+        dbConnection,
+      )
+      await createGenres(
+        [
+          { name: 'parent-one' },
+          { name: 'parent-two' },
+          { name: 'influence-one' },
+          { name: 'influence-two' },
+        ],
+        account.id,
+        dbConnection,
+      )
 
-        test.afterAll(async ({ dbConnection }) => {
-          await deleteAccounts([TEST_ACCOUNT.username], dbConnection)
-        })
+      await signInPage.goto()
+      await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
 
-        test.beforeEach(async ({ signInPage, createGenrePage, dbConnection }) => {
-          await createGenres(
-            [
-              { name: 'parent-one' },
-              { name: 'parent-two' },
-              { name: 'influence-one' },
-              { name: 'influence-two' },
-            ],
-            account.id,
-            dbConnection,
-          )
+      await createGenrePage.goto()
 
-          await signInPage.goto()
-          await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
-          await createGenrePage.goto()
-        })
+      await expect(createGenrePage.typeInput).toHaveText('Style')
+    })
 
-        test.afterEach(async ({ dbConnection }) => {
-          await deleteGenres(dbConnection)
-        })
+    test('when logged in with EDIT_GENRES permission, should create a genre with all fields filled', async ({
+      dbConnection,
+      signInPage,
+      createGenrePage,
+      genrePage,
+    }) => {
+      const [account] = await createAccounts(
+        [{ ...TEST_ACCOUNT, permissions: ['EDIT_GENRES'] }],
+        dbConnection,
+      )
+      await createGenres(
+        [
+          { name: 'parent-one' },
+          { name: 'parent-two' },
+          { name: 'influence-one' },
+          { name: 'influence-two' },
+        ],
+        account.id,
+        dbConnection,
+      )
 
-        test('should default to Style type', async ({ createGenrePage }) => {
-          await expect(createGenrePage.typeInput).toHaveText('Style')
-        })
+      await signInPage.goto()
+      await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
 
-        test('should create a genre with all fields filled', async ({
-          createGenrePage,
-          genrePage,
-        }) => {
-          const genre = {
-            name: 'created-genre',
-            subtitle: 'subtitle',
-            akas: {
-              primary: 'primary-one, primary-two',
-              secondary: 'secondary-one, secondary-two',
-              tertiary: 'tertiary-one, tertiary-two',
-            },
-            type: 'META',
-            parents: ['parent-one', 'parent-two'],
-            influences: ['influence-one', 'influence-two'],
-            relevance: 3,
-            shortDescription: 'short description',
-            longDescription: 'long description',
-            notes: 'notes',
-          } satisfies GenreFormData
+      await createGenrePage.goto()
 
-          await createGenrePage.createGenre(genre)
+      const genre = {
+        name: 'created-genre',
+        subtitle: 'subtitle',
+        akas: {
+          primary: 'primary-one, primary-two',
+          secondary: 'secondary-one, secondary-two',
+          tertiary: 'tertiary-one, tertiary-two',
+        },
+        type: 'META',
+        parents: ['parent-one', 'parent-two'],
+        influences: ['influence-one', 'influence-two'],
+        relevance: 3,
+        shortDescription: 'short description',
+        longDescription: 'long description',
+        notes: 'notes',
+      } satisfies GenreFormData
 
-          await genrePage.expectGenreData(genre)
-          await expect(genrePage.contributors).toHaveText(TEST_ACCOUNT.username)
-        })
+      await createGenrePage.createGenre(genre)
 
-        test('should create a history entry', async ({
-          createGenrePage,
-          genrePage,
-          genreHistoryPage,
-        }) => {
-          await createGenrePage.createGenre({ name: 'created-genre', parents: ['parent-one'] })
+      await genrePage.expectGenreData(genre)
+      await expect(genrePage.contributors).toHaveText(TEST_ACCOUNT.username)
+    })
 
-          await genrePage.historyButton.click()
+    test('when logged in with EDIT_GENRES permission, should create a history entry', async ({
+      dbConnection,
+      signInPage,
+      createGenrePage,
+      genrePage,
+      genreHistoryPage,
+    }) => {
+      const [account] = await createAccounts(
+        [{ ...TEST_ACCOUNT, permissions: ['EDIT_GENRES'] }],
+        dbConnection,
+      )
+      await createGenres(
+        [
+          { name: 'parent-one' },
+          { name: 'parent-two' },
+          { name: 'influence-one' },
+          { name: 'influence-two' },
+        ],
+        account.id,
+        dbConnection,
+      )
 
-          await expect(genreHistoryPage.entries).toHaveCount(1)
+      await signInPage.goto()
+      await signInPage.signIn(TEST_ACCOUNT.username, TEST_ACCOUNT.password)
 
-          await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).operation).toHaveText(
-            'Create',
-          )
-          await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).account).toHaveText(
-            TEST_ACCOUNT.username,
-          )
-          await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).name).toHaveText(
-            'created-genre',
-          )
-        })
-      })
+      await createGenrePage.goto()
+
+      await createGenrePage.createGenre({ name: 'created-genre', parents: ['parent-one'] })
+
+      await genrePage.historyButton.click()
+
+      await expect(genreHistoryPage.entries).toHaveCount(1)
+
+      await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).operation).toHaveText(
+        'Create',
+      )
+      await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).account).toHaveText(
+        TEST_ACCOUNT.username,
+      )
+      await expect(new GenreDiffEntry(genreHistoryPage.entries.nth(0)).name).toHaveText(
+        'created-genre',
+      )
     })
   })
 }
