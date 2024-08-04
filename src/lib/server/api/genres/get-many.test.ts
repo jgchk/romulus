4ba1f7a5f15afac9e2434e261448d35b4165efab
Jake Cheek <jake@f-m.fm>
2024-08-04 12:01:22 -0400

    test: Make test intents clearer

--- src/lib/server/api/genres/get-many.test.ts
+++ src/lib/server/api/genres/get-many.test.ts
@@ -1,17 +1,22 @@
 import { expect } from 'vitest'
 
 import { AccountsDatabase } from '$lib/server/db/controllers/accounts'
-import { GenresDatabase } from '$lib/server/db/controllers/genre'
+import { type ExtendedInsertGenre, GenresDatabase } from '$lib/server/db/controllers/genre'
 import { GenreHistoryDatabase } from '$lib/server/db/controllers/genre-history'
 import { createGenreHistoryEntry } from '$lib/server/genres'
 
 import { test } from '../../../../vitest-setup'
 import getManyGenres from './get-many'
 
+function getTestGenre(data?: Partial<ExtendedInsertGenre>): ExtendedInsertGenre {
+  return { name: 'Test', akas: [], parents: [], influencedBy: [], updatedAt: new Date(), ...data }
+}
+
 test('should return an empty array of genres when there are no genres in the DB', async ({
   dbConnection,
 }) => {
   const result = await getManyGenres({}, dbConnection)
+
   expect(result).toEqual({
     data: [],
     pagination: {
@@ -26,10 +31,7 @@ test('should return an array of genres when there are genres in the DB', async (
   dbConnection,
 }) => {
   const genresDb = new GenresDatabase()
-  await genresDb.insert(
-    [{ name: 'Test', akas: [], parents: [], influencedBy: [], updatedAt: new Date() }],
-    dbConnection,
-  )
+  await genresDb.insert([getTestGenre()], dbConnection)
 
   const result = await getManyGenres({}, dbConnection)
 
@@ -61,11 +63,11 @@ test('should paginate the results', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
     [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 3', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 4', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 5', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
+      getTestGenre({ name: 'Test 1' }),
+      getTestGenre({ name: 'Test 2' }),
+      getTestGenre({ name: 'Test 3' }),
+      getTestGenre({ name: 'Test 4' }),
+      getTestGenre({ name: 'Test 5' }),
     ],
     dbConnection,
   )
@@ -74,32 +76,8 @@ test('should paginate the results', async ({ dbConnection }) => {
 
   expect(result).toEqual({
     data: [
-      {
-        id: 3,
-        name: 'Test 3',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-      {
-        id: 4,
-        name: 'Test 4',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
+      expect.objectContaining({ name: 'Test 3' }),
+      expect.objectContaining({ name: 'Test 4' }),
     ],
     pagination: {
       skip: 2,
@@ -113,63 +91,16 @@ test('should handle a limit that is larger than the number of genres in the DB',
   dbConnection,
 }) => {
   const genresDb = new GenresDatabase()
-  await genresDb.insert(
-    [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 3', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-    ],
-    dbConnection,
-  )
+  await genresDb.insert([getTestGenre({ name: 'Test' })], dbConnection)
 
-  const result = await getManyGenres({ skip: 0, limit: 10 }, dbConnection)
+  const result = await getManyGenres({ limit: 10 }, dbConnection)
 
   expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-      {
-        id: 2,
-        name: 'Test 2',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-      {
-        id: 3,
-        name: 'Test 3',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-    ],
+    data: [expect.objectContaining({ name: 'Test' })],
     pagination: {
       skip: 0,
       limit: 10,
-      total: 3,
+      total: 1,
     },
   })
 })
@@ -178,42 +109,32 @@ test('should handle a skip that is larger than the number of genres in the DB', 
   dbConnection,
 }) => {
   const genresDb = new GenresDatabase()
-  await genresDb.insert(
-    [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 3', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-    ],
-    dbConnection,
-  )
-  const result = await getManyGenres({ skip: 10, limit: 2 }, dbConnection)
+  await genresDb.insert([getTestGenre()], dbConnection)
+
+  const result = await getManyGenres({ skip: 10 }, dbConnection)
+
   expect(result).toEqual({
     data: [],
     pagination: {
       skip: 10,
-      limit: 2,
-      total: 3,
+      limit: 10,
+      total: 1,
     },
   })
 })
 
 test('should handle a limit of 0', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
-  await genresDb.insert(
-    [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 3', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-    ],
-    dbConnection,
-  )
-  const result = await getManyGenres({ skip: 0, limit: 0 }, dbConnection)
+  await genresDb.insert([getTestGenre()], dbConnection)
+
+  const result = await getManyGenres({ limit: 0 }, dbConnection)
+
   expect(result).toEqual({
     data: [],
     pagination: {
       skip: 0,
       limit: 0,
-      total: 3,
+      total: 1,
     },
   })
 })
@@ -221,106 +142,53 @@ test('should handle a limit of 0', async ({ dbConnection }) => {
 test('should include parent ids when requested', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
-    [
-      { name: 'Test 1', akas: [], parents: [2], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-    ],
+    [getTestGenre({ name: 'Test 1', parents: [2] }), getTestGenre({ name: 'Test 2', parents: [] })],
     dbConnection,
   )
-  const result = await getManyGenres({ skip: 0, limit: 2, include: ['parents'] }, dbConnection)
-  expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-        parents: [2],
-      },
-      {
-        id: 2,
-        name: 'Test 2',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-        parents: [],
-      },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 2,
-      total: 2,
-    },
-  })
+
+  const result = await getManyGenres({ include: ['parents'] }, dbConnection)
+
+  expect(result.data).toEqual([
+    expect.objectContaining({
+      name: 'Test 1',
+      parents: [2],
+    }),
+    expect.objectContaining({
+      name: 'Test 2',
+      parents: [],
+    }),
+  ])
 })
 
 test('should include influencedBy ids when requested', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
     [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [2], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
+      getTestGenre({ name: 'Test 1', influencedBy: [2] }),
+      getTestGenre({ name: 'Test 2', influencedBy: [] }),
     ],
     dbConnection,
   )
-  const result = await getManyGenres({ skip: 0, limit: 2, include: ['influencedBy'] }, dbConnection)
-  expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-        influencedBy: [2],
-      },
-      {
-        id: 2,
-        name: 'Test 2',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-        influencedBy: [],
-      },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 2,
-      total: 2,
-    },
-  })
+
+  const result = await getManyGenres({ include: ['influencedBy'] }, dbConnection)
+
+  expect(result.data).toEqual([
+    expect.objectContaining({
+      name: 'Test 1',
+      influencedBy: [2],
+    }),
+    expect.objectContaining({
+      name: 'Test 2',
+      influencedBy: [],
+    }),
+  ])
 })
 
 test('should include akas when requested', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
     [
-      {
+      getTestGenre({
         name: 'Test 1',
         akas: [
           { relevance: 1, order: 1, name: 'tertiary-two' },
@@ -330,50 +198,41 @@ test('should include akas when requested', async ({ dbConnection }) => {
           { relevance: 3, order: 0, name: 'primary-one' },
           { relevance: 3, order: 1, name: 'primary-two' },
         ],
-        parents: [],
+      }),
+      getTestGenre({
+        name: 'Test 2',
         influencedBy: [],
-        updatedAt: new Date(),
-      },
+      }),
     ],
     dbConnection,
   )
-  const result = await getManyGenres({ skip: 0, limit: 2, include: ['akas'] }, dbConnection)
-  expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-        akas: {
-          primary: ['primary-one', 'primary-two'],
-          secondary: ['secondary-one', 'secondary-two'],
-          tertiary: ['tertiary-one', 'tertiary-two'],
-        },
+
+  const result = await getManyGenres({ include: ['akas'] }, dbConnection)
+
+  expect(result.data).toEqual([
+    expect.objectContaining({
+      name: 'Test 1',
+      akas: {
+        primary: ['primary-one', 'primary-two'],
+        secondary: ['secondary-one', 'secondary-two'],
+        tertiary: ['tertiary-one', 'tertiary-two'],
       },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 2,
-      total: 1,
-    },
-  })
+    }),
+    expect.objectContaining({
+      name: 'Test 2',
+      akas: {
+        primary: [],
+        secondary: [],
+        tertiary: [],
+      },
+    }),
+  ])
 })
 
 test('should allow filtering by creator account id', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   const [genre] = await genresDb.insert(
-    [
-      { name: 'Test 1', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-      { name: 'Test 2', akas: [], parents: [], influencedBy: [], updatedAt: new Date() },
-    ],
+    [getTestGenre({ name: 'Test 1' }), getTestGenre({ name: 'Test 2' })],
     dbConnection,
   )
 
@@ -392,195 +251,69 @@ test('should allow filtering by creator account id', async ({ dbConnection }) =>
     connection: dbConnection,
   })
 
-  const result = await getManyGenres(
-    { skip: 0, limit: 10, include: [], filter: { createdBy: account.id } },
-    dbConnection,
-  )
+  const result = await getManyGenres({ filter: { createdBy: account.id } }, dbConnection)
 
-  expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 10,
-      total: 1,
-    },
-  })
+  expect(result.data).toEqual([expect.objectContaining({ name: 'Test 1' })])
 })
 
 test('should allow filtering by empty shortDescription', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
     [
-      {
+      getTestGenre({
         name: 'Test 1',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: '',
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 2',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: null,
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 3',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: undefined,
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 4',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: 'Short',
-        updatedAt: new Date(),
-      },
+      }),
     ],
     dbConnection,
   )
 
-  const result = await getManyGenres(
-    { skip: 0, limit: 10, include: [], filter: { shortDescription: '' } },
-    dbConnection,
-  )
+  const result = await getManyGenres({ filter: { shortDescription: '' } }, dbConnection)
 
-  expect(result).toEqual({
-    data: [
-      {
-        id: 1,
-        name: 'Test 1',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: '',
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-      {
-        id: 2,
-        name: 'Test 2',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-      {
-        id: 3,
-        name: 'Test 3',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: null,
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 10,
-      total: 3,
-    },
-  })
+  expect(result.data).toEqual([
+    expect.objectContaining({ name: 'Test 1' }),
+    expect.objectContaining({ name: 'Test 2' }),
+    expect.objectContaining({ name: 'Test 3' }),
+  ])
 })
 
 test('should allow filtering by non-empty shortDescription', async ({ dbConnection }) => {
   const genresDb = new GenresDatabase()
   await genresDb.insert(
     [
-      {
+      getTestGenre({
         name: 'Test 1',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: '',
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 2',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: null,
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 3',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: undefined,
-        updatedAt: new Date(),
-      },
-      {
+      }),
+      getTestGenre({
         name: 'Test 4',
-        akas: [],
-        parents: [],
-        influencedBy: [],
         shortDescription: 'Short',
-        updatedAt: new Date(),
-      },
+      }),
     ],
     dbConnection,
   )
 
-  const result = await getManyGenres(
-    { skip: 0, limit: 10, include: [], filter: { shortDescription: 'Short' } },
-    dbConnection,
-  )
+  const result = await getManyGenres({ filter: { shortDescription: 'Short' } }, dbConnection)
 
-  expect(result).toEqual({
-    data: [
-      {
-        id: 4,
-        name: 'Test 4',
-        subtitle: null,
-        type: 'STYLE',
-        nsfw: false,
-        shortDescription: 'Short',
-        longDescription: null,
-        notes: null,
-        relevance: 99,
-        createdAt: expect.any(Date) as Date,
-        updatedAt: expect.any(Date) as Date,
-      },
-    ],
-    pagination: {
-      skip: 0,
-      limit: 10,
-      total: 1,
-    },
-  })
+  expect(result.data).toEqual([expect.objectContaining({ name: 'Test 4' })])
 })
