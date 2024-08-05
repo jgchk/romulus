bc73cd2291234fc470579bd8316f6e2f5cc57b68
Jake Cheek <jake@f-m.fm>
2024-08-05 09:17:41 -0400

    feat: Throw error if account id does not exist

--- src/routes/accounts/[id]/apps/+page.server.ts
+++ src/routes/accounts/[id]/apps/+page.server.ts
@@ -1,14 +1,29 @@
 import { error } from '@sveltejs/kit'
 import { z } from 'zod'
 
+import { AccountsDatabase } from '$lib/server/db/controllers/accounts'
+
 import type { PageServerLoad, PageServerLoadEvent } from './$types'
 
-export const load = (({ params }: Pick<PageServerLoadEvent, 'params'>) => {
+export const load = (async ({
+  params,
+  locals,
+}: {
+  params: PageServerLoadEvent['params']
+  locals: Pick<App.Locals, 'dbConnection'>
+}) => {
   const maybeId = z.coerce.number().int().safeParse(params.id)
   if (!maybeId.success) {
     return error(400, 'Invalid account ID')
   }
-  // const id = maybeId.data
+  const id = maybeId.data
+
+  const accountsDb = new AccountsDatabase()
+  const maybeAccount = await accountsDb.findById(id, locals.dbConnection)
+  if (!maybeAccount) {
+    return error(404, 'Account not found')
+  }
+  // const account = maybeAccount
 
   return { keys: [] }
 }) satisfies PageServerLoad
--- src/routes/accounts/[id]/apps/page.server.test.ts
+++ src/routes/accounts/[id]/apps/page.server.test.ts
@@ -3,11 +3,20 @@ import { expect } from 'vitest'
 import { test } from '../../../../vitest-setup'
 import { load } from './+page.server'
 
-test('should throw error if account id is not a number', () => {
+test('should throw error if account id is not a number', async ({ dbConnection }) => {
   try {
-    load({ params: { id: 'test' } })
+    await load({ params: { id: 'test' }, locals: { dbConnection } })
     expect.fail('should throw error')
   } catch (e) {
     expect(e).toEqual({ status: 400, body: { message: 'Invalid account ID' } })
+  }
+})
+
+test('should throw error if account does not exist', async ({ dbConnection }) => {
+  try {
+    await load({ params: { id: '1' }, locals: { dbConnection } })
+    expect.fail('should throw error')
+  } catch (e) {
+    expect(e).toEqual({ status: 404, body: { message: 'Account not found' } })
   }
 })
