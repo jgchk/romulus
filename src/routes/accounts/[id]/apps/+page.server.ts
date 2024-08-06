bf2911818138b53a624386c60e1e39a2bf4fb42d
Jake Cheek <jake@f-m.fm>
2024-08-06 07:05:42 -0400

    feat: Throw errors on incorrect api key delete calls

--- src/routes/accounts/[id]/apps/+page.server.ts
+++ src/routes/accounts/[id]/apps/+page.server.ts
@@ -100,4 +100,36 @@ export const actions = {
 
     return { success: true, id: insertedKey.id, name, key }
   },
+
+  delete: async ({
+    params,
+    locals,
+  }: {
+    params: RequestEvent['params']
+    locals: {
+      dbConnection: App.Locals['dbConnection']
+      user: Pick<NonNullable<App.Locals['user']>, 'id'> | undefined
+    }
+  }) => {
+    if (!locals.user) {
+      return error(401, 'Unauthorized')
+    }
+
+    const maybeId = z.coerce.number().int().safeParse(params.id)
+    if (!maybeId.success) {
+      return error(400, 'Invalid account ID')
+    }
+    const id = maybeId.data
+
+    if (locals.user.id !== id) {
+      return error(401, 'Unauthorized')
+    }
+
+    const accountsDb = new AccountsDatabase()
+    const maybeAccount = await accountsDb.findById(id, locals.dbConnection)
+    if (!maybeAccount) {
+      return error(404, 'Account not found')
+    }
+    const account = maybeAccount
+  },
 } satisfies Actions
--- src/routes/accounts/[id]/apps/page.server.test.ts
+++ src/routes/accounts/[id]/apps/page.server.test.ts
@@ -234,3 +234,55 @@ describe('create', () => {
     expect(res).toEqual({ status: 400, data: { errors: { name: ['Name is required'] } } })
   })
 })
+
+describe('delete', () => {
+  test('should throw error if not logged in', async ({ dbConnection }) => {
+    try {
+      await actions.delete({
+        params: { id: '1' },
+        locals: { dbConnection, user: undefined },
+      })
+      expect.fail('should throw error')
+    } catch (e) {
+      expect(e).toEqual({ status: 401, body: { message: 'Unauthorized' } })
+    }
+  })
+
+  test('should throw error if account is not the one currently logged in', async ({
+    dbConnection,
+  }) => {
+    try {
+      await actions.delete({
+        params: { id: '1' },
+        locals: { dbConnection, user: { id: 2 } },
+      })
+      expect.fail('should throw error')
+    } catch (e) {
+      expect(e).toEqual({ status: 401, body: { message: 'Unauthorized' } })
+    }
+  })
+
+  test('should throw error if account id is not a number', async ({ dbConnection }) => {
+    try {
+      await actions.delete({
+        params: { id: 'test' },
+        locals: { dbConnection, user: { id: 1 } },
+      })
+      expect.fail('should throw error')
+    } catch (e) {
+      expect(e).toEqual({ status: 400, body: { message: 'Invalid account ID' } })
+    }
+  })
+
+  test('should throw error if account does not exist', async ({ dbConnection }) => {
+    try {
+      await actions.delete({
+        params: { id: '1' },
+        locals: { dbConnection, user: { id: 1 } },
+      })
+      expect.fail('should throw error')
+    } catch (e) {
+      expect(e).toEqual({ status: 404, body: { message: 'Account not found' } })
+    }
+  })
+})
