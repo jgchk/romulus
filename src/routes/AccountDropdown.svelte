f5173a9d1990940d0ff151252b4dd0726bdd10fd
Jake Cheek <jake@f-m.fm>
2024-08-07 08:02:59 -0400

    fix: Close profile dropdown when clicking main button

--- src/routes/AccountDropdown.svelte
+++ src/routes/AccountDropdown.svelte
@@ -29,71 +29,71 @@
   }}
 />
 
-<button
-  use:popoverReference
-  class="group/account-button h-full w-full"
-  on:click={() => (open = !open)}
->
-  <div
-    class="flex h-full items-center gap-1 rounded px-2 pr-1 transition group-hover/account-button:bg-gray-200 dark:group-hover/account-button:bg-gray-800"
+<div use:clickOutside={() => (open = false)}>
+  <button
+    use:popoverReference
+    class="group/account-button h-full w-full"
+    on:click={() => (open = !open)}
   >
-    <div>{account.username}</div>
-    <CaretDown
-      size={12}
-      weight="bold"
-      class={cn('text-gray-500 transition-all dark:text-gray-400', open && 'rotate-180 transform')}
-    />
-  </div>
-</button>
+    <div
+      class="flex h-full items-center gap-1 rounded px-2 pr-1 transition group-hover/account-button:bg-gray-200 dark:group-hover/account-button:bg-gray-800"
+    >
+      <div>{account.username}</div>
+      <CaretDown
+        size={12}
+        weight="bold"
+        class={cn(
+          'text-gray-500 transition-all dark:text-gray-400',
+          open && 'rotate-180 transform',
+        )}
+      />
+    </div>
+  </button>
 
-{#if open}
-  <div
-    class="relative z-10"
-    transition:slide|local={{ axis: 'y' }}
-    use:popoverElement
-    use:clickOutside={() => (open = false)}
-  >
-    <Card class="overflow-hidden shadow">
-      <a
-        class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
-        href="/accounts/{account.id}"
-      >
-        <UserCircle size={18} />
-        Profile
-      </a>
+  {#if open}
+    <div class="relative z-10" transition:slide|local={{ axis: 'y' }} use:popoverElement>
+      <Card class="overflow-hidden shadow">
+        <a
+          class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
+          href="/accounts/{account.id}"
+        >
+          <UserCircle size={18} />
+          Profile
+        </a>
 
-      <a
-        class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
-        href="/accounts/{account.id}/keys"
-      >
-        <Key size={18} />
-        API Keys
-      </a>
+        <a
+          class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
+          href="/accounts/{account.id}/keys"
+        >
+          <Key size={18} />
+          API Keys
+        </a>
 
-      <button
-        class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
-        on:click={() => {
-          userSettings.update((prev) => ({ ...prev, darkMode: !$userSettings.darkMode }))
-        }}
-      >
-        {#if $userSettings.darkMode}
-          <MoonStars size={18} />
-          Dark Mode
-        {:else}
-          <Sun size={18} />
-          Light Mode
-        {/if}
-      </button>
-
-      <form method="POST" action="/sign-out" use:enhance>
         <button
           class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
-          type="submit"
+          on:click={() => {
+            userSettings.update((prev) => ({ ...prev, darkMode: !$userSettings.darkMode }))
+          }}
         >
-          <SignOut size={18} />
-          Sign out
+          {#if $userSettings.darkMode}
+            <MoonStars size={18} />
+            Dark Mode
+          {:else}
+            <Sun size={18} />
+            Light Mode
+          {/if}
         </button>
-      </form>
-    </Card>
-  </div>
-{/if}
+
+        <form method="POST" action="/sign-out" use:enhance>
+          <button
+            class="flex w-full items-center justify-start gap-1.5 text-nowrap px-2 py-1.5 text-gray-600 transition hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-gray-800"
+            type="submit"
+          >
+            <SignOut size={18} />
+            Sign out
+          </button>
+        </form>
+      </Card>
+    </div>
+  {/if}
+</div>
