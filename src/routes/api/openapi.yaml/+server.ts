e1fcc7887e66f2ca058cd0263c892795657152b5
Jake Cheek <jake@f-m.fm>
2024-08-10 09:25:43 -0400

    feat: Dynamically generate OpenAPI servers depending on whether running locally or in prod

---
+++ src/routes/api/openapi.yaml/+server.ts
@@ -0,0 +1,20 @@
+import openApiDoc from '../../../../docs/openapi.yaml?raw'
+import type { RequestHandler } from './$types'
+
+export const GET: RequestHandler = ({ url }) => {
+  let doc = openApiDoc
+
+  const isLocalhost = url.hostname === 'localhost'
+  if (isLocalhost) {
+    doc = openApiDoc.replace(
+      '  - url: https://www.romulus.lol/api',
+      '  - url: http://localhost:5173/api\n  - url: https://www.romulus.lol/api',
+    )
+  }
+
+  return new Response(doc, {
+    headers: {
+      'Content-Type': 'application/yaml',
+    },
+  })
+}
