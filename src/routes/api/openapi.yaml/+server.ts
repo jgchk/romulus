c9b2cf547b8c135806dd2c34aec2b85b4afb4c80
Jake Cheek <jake@f-m.fm>
2024-08-10 12:02:25 -0400

    feat: Support all hosts in dynamic server generation in OpenAPI doc

--- src/routes/api/openapi.yaml/+server.ts
+++ src/routes/api/openapi.yaml/+server.ts
@@ -2,15 +2,10 @@ import openApiDoc from '../../../../docs/openapi.yaml?raw'
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = ({ url }) => {
-  let doc = openApiDoc
-
-  const isLocalhost = url.hostname === 'localhost'
-  if (isLocalhost) {
-    doc = openApiDoc.replace(
-      '  - url: https://www.romulus.lol/api',
-      `  - url: http://${url.host}/api\n  - url: https://www.romulus.lol/api`,
-    )
-  }
+  const doc = openApiDoc.replace(
+    '  - url: https://www.romulus.lol/api',
+    `  - url: ${url.protocol}//${url.host}/api`,
+  )
 
   return new Response(doc, {
     headers: {
