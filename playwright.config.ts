150917aefb5245e0527f7dbfcf011935ee257050
Jake Cheek <jake@f-m.fm>
2024-08-10 11:12:15 -0400

    chore: Remove unused import

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices, type PlaywrightTestConfig } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test'
 import dotenv from 'dotenv'
 
 dotenv.config({ path: '.env.test' })
