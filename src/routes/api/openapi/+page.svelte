8a82fc643fc537a68073b70d781a763e3842c9bb
Jake Cheek <jake@f-m.fm>
2024-08-10 09:02:57 -0400

    feat: Add OpenAPI explorer page

---
+++ src/routes/api/openapi/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import 'swagger-ui/dist/swagger-ui.css'
+
+  import { onMount } from 'svelte'
+  import SwaggerUI from 'swagger-ui'
+
+  import Card from '$lib/atoms/Card.svelte'
+
+  onMount(() => {
+    SwaggerUI({
+      url: '/api/openapi.yaml',
+      dom_id: '#swagger-ui-container',
+    })
+  })
+</script>
+
+<svelte:head>
+  <title>OpenAPI Spec</title>
+</svelte:head>
+
+<Card class="h-full w-full overflow-auto bg-white dark:bg-white">
+  <div id="swagger-ui-container" />
+</Card>
