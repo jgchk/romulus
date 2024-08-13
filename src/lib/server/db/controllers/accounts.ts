ce1ecd6e683c06335dd062b2640b451a047051e4
Jake Cheek <jake@f-m.fm>
2024-08-13 11:53:58 -0400

    refactor: Remove indirection around IAccountsDatabase interface

--- src/lib/server/db/controllers/accounts.ts
+++ src/lib/server/db/controllers/accounts.ts
@@ -5,33 +5,31 @@ import { hasUpdate, makeUpdate } from '$lib/utils/db'
 import type { IDrizzleConnection } from '../connection'
 import { type Account, accounts, type InsertAccount } from '../schema'
 
-export type IAccountsDatabase<T> = {
-  insert: (data: InsertAccount[], conn: T) => Promise<Account[]>
-  findById: (id: Account['id'], conn: T) => Promise<Account | undefined>
-  findByUsername: (username: Account['username'], conn: T) => Promise<Account | undefined>
-  update: (id: number, update: Partial<InsertAccount>, conn: T) => Promise<Account>
-  deleteByUsername: (usernames: Account['username'][], conn: T) => Promise<void>
-  deleteAll: (conn: T) => Promise<void>
-}
-
-export class AccountsDatabase implements IAccountsDatabase<IDrizzleConnection> {
-  insert(data: InsertAccount[], conn: IDrizzleConnection) {
+export class AccountsDatabase {
+  insert(data: InsertAccount[], conn: IDrizzleConnection): Promise<Account[]> {
     return conn.insert(accounts).values(data).returning()
   }
 
-  findById(id: Account['id'], conn: IDrizzleConnection) {
+  findById(id: Account['id'], conn: IDrizzleConnection): Promise<Account | undefined> {
     return conn.query.accounts.findFirst({
       where: eq(accounts.id, id),
     })
   }
 
-  findByUsername(username: Account['username'], conn: IDrizzleConnection) {
+  findByUsername(
+    username: Account['username'],
+    conn: IDrizzleConnection,
+  ): Promise<Account | undefined> {
     return conn.query.accounts.findFirst({
       where: eq(accounts.username, username),
     })
   }
 
-  async update(id: number, update: Partial<InsertAccount>, conn: IDrizzleConnection) {
+  async update(
+    id: number,
+    update: Partial<InsertAccount>,
+    conn: IDrizzleConnection,
+  ): Promise<Account> {
     if (!hasUpdate(update)) {
       const account = await this.findById(id, conn)
       if (!account) throw new Error(`Account not found: ${id}`)
@@ -47,12 +45,15 @@ export class AccountsDatabase implements IAccountsDatabase<IDrizzleConnection> {
     return account
   }
 
-  async deleteByUsername(usernames: Account['username'][], conn: IDrizzleConnection) {
+  async deleteByUsername(
+    usernames: Account['username'][],
+    conn: IDrizzleConnection,
+  ): Promise<void> {
     if (usernames.length === 0) return
     await conn.delete(accounts).where(inArray(accounts.username, usernames))
   }
 
-  async deleteAll(conn: IDrizzleConnection) {
+  async deleteAll(conn: IDrizzleConnection): Promise<void> {
     await conn.delete(accounts)
   }
 }
