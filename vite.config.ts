df7a3b3290b9a45fa8d7dc6b1de5cd171422c490
Jake Cheek <jake@f-m.fm>
2024-08-13 09:49:55 -0400

    build: Add phosphor preprocessor to improve build times

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [sveltekit()],
   optimizeDeps: {
-    exclude: ['oslo'],
+    exclude: ['oslo', 'phosphor-svelte'],
   },
 })
