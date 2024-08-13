4c39d20fb1bf06f9f62b9dc829f0ccc0856b17e0
Jake Cheek <jake@f-m.fm>
2024-08-13 11:55:23 -0400

    refactor: Remove indirection around IGenreInfluencesDatabase interface

--- src/lib/server/db/controllers/genre-influences.ts
+++ src/lib/server/db/controllers/genre-influences.ts
@@ -1,12 +1,8 @@
 import type { IDrizzleConnection } from '../connection'
 import { type GenreInfluence, genreInfluences, type InsertGenreInfluence } from '../schema'
 
-export type IGenreInfluencesDatabase<T> = {
-  insert: (data: InsertGenreInfluence[], conn: T) => Promise<GenreInfluence[]>
-}
-
-export class GenreInfluencesDatabase implements IGenreInfluencesDatabase<IDrizzleConnection> {
-  insert(data: InsertGenreInfluence[], conn: IDrizzleConnection) {
+export class GenreInfluencesDatabase {
+  insert(data: InsertGenreInfluence[], conn: IDrizzleConnection): Promise<GenreInfluence[]> {
     return conn.insert(genreInfluences).values(data).returning()
   }
 }
