e6f3e4ef4eb541d8d57f0f5ce38c2ab967cce8c5
Jake Cheek <jake@f-m.fm>
2024-09-02 17:44:48 -0400

    feat: create dockerfile

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { phosphorSvelteOptimize } from 'phosphor-svelte/preprocessor'
 
@@ -9,9 +9,6 @@ const config = {
   preprocess: [phosphorSvelteOptimize(), vitePreprocess()],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
 }
