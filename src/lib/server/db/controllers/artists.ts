cc8478ae807c61b6aeafd995bc3a1da0a7f5faa9
Jake Cheek <jake@f-m.fm>
2024-09-12 06:56:03 -0400

    chore: remove unused db function

--- src/lib/server/db/controllers/artists.ts
+++ src/lib/server/db/controllers/artists.ts
@@ -1,8 +1,8 @@
 import type { SQL } from 'drizzle-orm'
-import { and, asc, count, desc, eq, inArray } from 'drizzle-orm'
+import { and, asc, count, desc, inArray } from 'drizzle-orm'
 
 import type { IDrizzleConnection } from '../connection'
-import { type Artist, artists, type InsertArtist, type Release, type Track } from '../schema'
+import { type Artist, artists, type Release, type Track } from '../schema'
 
 export const FIND_MANY_INCLUDE = ['releases', 'releases-full', 'tracks', 'tracks-full'] as const
 export type FindInclude = (typeof FIND_MANY_INCLUDE)[number]
@@ -32,65 +32,6 @@ export type FindArtist<T extends FindInclude> = Artist & {
 }
 
 export default class ArtistsDatabase {
-  async insert(data: InsertArtist[], conn: IDrizzleConnection): Promise<Artist[]> {
-    const results = await conn.insert(artists).values(data).returning()
-    return results
-  }
-
-  async findById<I extends FindInclude>(
-    id: number,
-    { include = [] }: { include?: I[] },
-    conn: IDrizzleConnection,
-  ): Promise<FindArtist<I> | undefined> {
-    const includeReleases = (include as string[]).includes('releases')
-    const includeReleasesFull = (include as string[]).includes('releases-full')
-    const includeTracks = (include as string[]).includes('tracks')
-    const includeTracksFull = (include as string[]).includes('tracks-full')
-
-    const result = await conn.query.artists.findFirst({
-      where: eq(artists.id, id),
-      with: {
-        releases: includeReleasesFull
-          ? { columns: {}, with: { release: true } }
-          : includeReleases
-            ? { columns: { releaseId: true } }
-            : undefined,
-        tracks: includeTracksFull
-          ? { columns: {}, with: { track: true } }
-          : includeTracks
-            ? { columns: { trackId: true } }
-            : undefined,
-      },
-    })
-
-    if (!result) {
-      return undefined
-    }
-
-    const output = result
-    if (includeReleasesFull) {
-      // @ts-expect-error - we are dynamically adding a new field
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return
-      output.releases = result.releases.map((r) => r.release)
-    } else if (includeReleases) {
-      // @ts-expect-error - we are dynamically adding a new field
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return
-      output.releases = result.releases.map((r) => r.releaseId)
-    }
-    if (includeTracksFull) {
-      // @ts-expect-error - we are dynamically adding a new field
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return
-      output.tracks = result.tracks.map((t) => t.track)
-    } else if (includeTracks) {
-      // @ts-expect-error - we are dynamically adding a new field
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-return
-      output.tracks = result.tracks.map((t) => t.trackId)
-    }
-
-    // @ts-expect-error - we are dynamically adding new fields
-    return output
-  }
-
   async findMany<I extends FindInclude = never>(
     { skip, limit, include = [], filter = {}, sort = {} }: FindManyParams<I>,
     conn: IDrizzleConnection,
