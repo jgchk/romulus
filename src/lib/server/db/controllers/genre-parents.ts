571cc3be81d05e2f47b3720afdea9d6919f32903
Jake Cheek <jake@f-m.fm>
2024-11-04 13:58:40 -0500

    refactor: remove GenreParentDatabase.findByParentId()

--- src/lib/server/db/controllers/genre-parents.ts
+++ src/lib/server/db/controllers/genre-parents.ts
@@ -1,19 +1,8 @@
-import { eq } from 'drizzle-orm'
-
 import type { IDrizzleConnection } from '../connection'
 import { type GenreParent, genreParents, type InsertGenreParent } from '../schema'
 
 export class GenreParentsDatabase {
   insert(data: InsertGenreParent[], conn: IDrizzleConnection): Promise<GenreParent[]> {
     return conn.insert(genreParents).values(data).returning()
-  }
-
-  findByParentId(
-    parentId: GenreParent['parentId'],
-    conn: IDrizzleConnection,
-  ): Promise<GenreParent[]> {
-    return conn.query.genreParents.findMany({
-      where: eq(genreParents.parentId, parentId),
-    })
   }
 }
