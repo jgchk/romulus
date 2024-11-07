14c4da01cc39d22b00036dfdff8aa11fea95cbf5
Jake Cheek <jake@f-m.fm>
2024-11-07 07:12:38 -0500

    refactor: inline GenreHistoryDatabase.findAll()

--- src/lib/server/db/controllers/genre-history.ts
+++ src/lib/server/db/controllers/genre-history.ts
@@ -1,4 +1,4 @@
-import { and, count, eq, inArray } from 'drizzle-orm'
+import { inArray } from 'drizzle-orm'
 
 import type { GenreOperation } from '$lib/types/genres'
 
@@ -15,35 +15,6 @@ export type FindAllParams = {
 }
 
 export class GenreHistoryDatabase {
-  async findAll(
-    { skip, limit, filter = {} }: FindAllParams,
-    conn: IDrizzleConnection,
-  ): Promise<{ results: GenreHistory[]; total: number }> {
-    const wheres = []
-    if (filter.accountId !== undefined) {
-      wheres.push(eq(genreHistory.accountId, filter.accountId))
-    }
-    if (filter.operation !== undefined) {
-      wheres.push(eq(genreHistory.operation, filter.operation))
-    }
-    const where = wheres.length > 0 ? and(...wheres) : undefined
-
-    const dataQuery = conn.query.genreHistory.findMany({
-      where,
-      offset: skip,
-      limit,
-    })
-    const totalQuery = conn.select({ total: count() }).from(genreHistory).where(where).$dynamic()
-
-    const queryResults = limit === 0 ? [] : await dataQuery.execute()
-    const totalResults = await totalQuery.execute()
-
-    return {
-      results: queryResults,
-      total: totalResults.length > 0 ? totalResults[0].total : 0,
-    }
-  }
-
   async deleteByGenreIds(
     genreIds: GenreHistory['treeGenreId'][],
     conn: IDrizzleConnection,
