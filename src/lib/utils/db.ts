9c31b23071d67afe2a42523bd46a895e4636c85d
Jake Cheek <jake@f-m.fm>
2024-11-08 15:17:24 -0500

    chore: remove unused files

--- src/lib/utils/db.ts
+++
@@ -1,6 +0,0 @@
-export type UpdateData<T> = Partial<Omit<T, 'id'>>
-export const makeUpdate = <T extends UpdateData<T>>(data: T): T =>
-  Object.fromEntries(Object.entries(data).filter(([, value]) => value !== undefined)) as T
-
-export const hasUpdate = <T extends UpdateData<T>>(data: T): boolean =>
-  Object.values(data).some((value) => value !== undefined)
