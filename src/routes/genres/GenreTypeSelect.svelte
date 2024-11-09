4f9c90aabb96863fecfcbf8972aecfc7a04bb37e
Jake Cheek <jake.cheek@itential.com>
2024-11-09 18:25:53 -0500

    refactor: update GenreTypeSelect to svelte 5

--- src/routes/genres/GenreTypeSelect.svelte
+++ src/routes/genres/GenreTypeSelect.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-  import { type ComponentProps, createEventDispatcher } from 'svelte'
+  import { type ComponentProps } from 'svelte'
 
-  import type { Option } from '$lib/atoms/Select'
   import Select from '$lib/atoms/Select.svelte'
   import { GENRE_TYPES, type GenreType, GenreTypeNames } from '$lib/types/genres'
 
-  type $$Props = Omit<ComponentProps<Select<GenreType>>, 'options'>
+  type Props = Omit<ComponentProps<Select<GenreType>>, 'options'>
 
-  export let value: $$Props['value'] = undefined
+  let { value = $bindable(undefined), ...rest }: Props = $props()
 
   const options = GENRE_TYPES.map((type) => ({ value: type, label: GenreTypeNames[type] }))
-
-  const dispatch = createEventDispatcher<{ change: Option<GenreType> }>()
 </script>
 
-<Select bind:value {options} onChange={(option) => dispatch('change', option)} {...$$restProps} />
+<Select bind:value {options} {...rest} />
