d0a8b261fcb09dce496cdd3da7699c8b97c567e8
Jake Cheek <jake@f-m.fm>
2024-11-09 09:12:10 -0500

    refactor: update RomcodeNode to svelte 5

--- src/lib/components/Romcode/RomcodeNode.svelte
+++ src/lib/components/Romcode/RomcodeNode.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
   import RomcodeGenreLink from './RomcodeGenreLink.svelte'
+  import RomcodeNode from './RomcodeNode.svelte'
   import type { Node } from './types'
 
-  export let node: Node
-  export let genres: { id: number; name: string; nsfw: boolean }[]
+  type Props = {
+    node: Node
+    genres: { id: number; name: string; nsfw: boolean }[]
+  }
+
+  let { node, genres }: Props = $props()
 </script>
 
 {#if node.type === 'Root'}
   {#each node.children as child}
-    <svelte:self node={child} {genres} />
+    <RomcodeNode node={child} {genres} />
   {/each}
 {:else if node.type === 'Paragraph'}
   <p class="mb-3 leading-relaxed text-gray-700 transition last:mb-0 dark:text-gray-300">
     {#each node.children as child}
-      <svelte:self node={child} {genres} />
+      <RomcodeNode node={child} {genres} />
     {/each}
   </p>
 {:else if node.type === 'Text'}
@@ -21,13 +26,13 @@
 {:else if node.type === 'Bold'}
   <strong>
     {#each node.children as child}
-      <svelte:self node={child} {genres} />
+      <RomcodeNode node={child} {genres} />
     {/each}
   </strong>
 {:else if node.type === 'Italic'}
   <em>
     {#each node.children as child}
-      <svelte:self node={child} {genres} />
+      <RomcodeNode node={child} {genres} />
     {/each}
   </em>
 {:else if node.type === 'Link'}
