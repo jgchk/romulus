3af00e9e5f2a2ab026e0708c8b410c1c2de08a57
Jake Cheek <jake@f-m.fm>
2024-11-23 10:01:22 -0500

    chore: remove unused branch manager code

--- src/lib/server/features/media/branches/domain/branches/events.ts
+++
@@ -1,9 +0,0 @@
-export type MediaTypeBranchesEvent = MediaTypeBranchCreatedEvent
-
-export class MediaTypeBranchCreatedEvent {
-  constructor(
-    public readonly id: string,
-    public readonly name: string,
-    public readonly baseBranchId: string | undefined,
-  ) {}
-}
