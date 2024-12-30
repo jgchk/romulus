a0edd66f2129b70fa34b397da4e2460e65cc11bf
Jake Cheek <jake.cheek@itential.com>
2024-12-30 11:35:56 -0500

    refactor: move media artifacts stuff into command side subdirectory

--- services/media/src/artifacts/commands/domain/errors.ts
+++ services/media/src/artifacts/commands/domain/errors.ts
@@ -1,4 +1,4 @@
-import { CustomError } from '../../shared/domain/errors'
+import { CustomError } from '../../../shared/domain/errors'
 
 export class MediaArtifactSchemaNotFoundError extends CustomError {
   constructor(public readonly id: string) {
