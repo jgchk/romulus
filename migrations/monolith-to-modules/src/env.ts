601eb0d9180109ebcb5bd7dea997ae9d7c30b684
Jake Cheek <jake.cheek@itential.com>
2025-03-05 06:52:42 -0500

    feat: add rest of tables to migration script

--- migrations/monolith-to-modules/src/env.ts
+++ migrations/monolith-to-modules/src/env.ts
@@ -3,6 +3,9 @@ import { type } from 'arktype'
 const envSchema = type({
   MONOLITH_DATABASE_URL: 'string > 0',
   AUTHENTICATION_DATABASE_URL: 'string > 0',
+  AUTHORIZATION_DATABASE_URL: 'string > 0',
+  USER_SETTINGS_DATABASE_URL: 'string > 0',
+  GENRES_DATABASE_URL: 'string > 0',
 })
 
 const envResult = envSchema(process.env)
--- migrations/monolith-to-modules/src/main.ts
+++ migrations/monolith-to-modules/src/main.ts
@@ -1,19 +1,59 @@
-import { pg } from '@romulus/authentication/infrastructure'
+import { pg as authenticationPg } from '@romulus/authentication/infrastructure'
+import { pg as authorizationPg } from '@romulus/authorization/infrastructure'
+import { pg as genresPg } from '@romulus/genres/infrastructure'
+import { pg as userSettingsPg } from '@romulus/user-settings/infrastructure'
 import postgres from 'postgres'
 
 import { env } from './env.js'
 import { migrateAccounts } from './modules/accounts.js'
+import { migrateGenres } from './modules/genres.js'
+import { migratePermissions } from './modules/permissions.js'
+import { migrateUserSettings } from './modules/user-settings.js'
 
 async function main() {
-  const pg_ = postgres(env.AUTHENTICATION_DATABASE_URL)
-  const drizzle = pg.getDbConnection(pg_)
-
+  const authenticationDrizzle = authenticationPg.getDbConnection(
+    postgres(env.AUTHENTICATION_DATABASE_URL),
+  )
   await migrateAccounts(
     async () => {
-      await pg.migrate(drizzle)
+      await authenticationPg.migrate(authenticationDrizzle)
     },
     async (query) => {
-      await drizzle.execute(query)
+      await authenticationDrizzle.execute(query)
+    },
+  )
+
+  const authorizationDrizzle = authorizationPg.getDbConnection(
+    postgres(env.AUTHORIZATION_DATABASE_URL),
+  )
+  await migratePermissions(
+    async () => {
+      await authorizationPg.migrate(authorizationDrizzle)
+    },
+    async (query) => {
+      await authorizationDrizzle.execute(query)
+    },
+  )
+
+  const userSettingsDrizzle = userSettingsPg.getDbConnection(
+    postgres(env.USER_SETTINGS_DATABASE_URL),
+  )
+  await migrateUserSettings(
+    async () => {
+      await userSettingsPg.migrate(userSettingsDrizzle)
+    },
+    async (query) => {
+      await userSettingsDrizzle.execute(query)
+    },
+  )
+
+  const genresDrizzle = genresPg.getDbConnection(postgres(env.GENRES_DATABASE_URL))
+  await migrateGenres(
+    async () => {
+      await genresPg.migrate(genresDrizzle)
+    },
+    async (query) => {
+      await genresDrizzle.execute(query)
     },
   )
 }
