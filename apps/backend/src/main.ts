3cc98c25619df9fb6f585e7d1c0624d15b90bafc
Jake Cheek <jake@f-m.fm>
2025-03-09 12:02:40 -0400

    refactor: inline setupAdminAccountForDevelopment

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -32,11 +32,9 @@ import {
   type GenresApplication,
   type MediaApplication,
 } from './application.js'
-import { setupAdminAccountForDevelopment as _setupAdminAccountForDevelopment } from './dev.js'
 
 export async function main({
   config,
-  setupAdminAccountForDevelopment = _setupAdminAccountForDevelopment,
 }: {
   config: {
     authenticationDatabaseUrl: string
@@ -46,7 +44,6 @@ export async function main({
 
     enableDevAdminAccount: boolean
   }
-  setupAdminAccountForDevelopment?: typeof _setupAdminAccountForDevelopment
 }) {
   const {
     authenticationInfrastructure,
@@ -262,4 +259,46 @@ async function setupRoles(authorizationApplication: AuthorizationApplication) {
   if (result.isErr()) {
     console.error(result.error)
   }
+}
+
+async function setupAdminAccountForDevelopment({
+  authentication,
+  authorization,
+}: {
+  authentication: AuthenticationApplication
+  authorization: AuthorizationApplication
+}) {
+  const admin = await ensureAdminAccountExists(authentication)
+  if (admin.isErr()) {
+    console.error('Error ensuring admin account exists', admin.error)
+    return
+  }
+
+  const result = await assignAllRolesToAccount(authorization, admin.value.id)
+  if (result.isErr()) {
+    console.error('Error assigning roles to admin account', result.error)
+  }
+}
+
+async function ensureAdminAccountExists(authentication: AuthenticationApplication) {
+  const registerResult = await authentication.registerCommand().execute('admin', 'admin')
+  if (!(registerResult instanceof Error)) return ok({ id: registerResult.newUserAccount.id })
+
+  const loginResult = await authentication.loginCommand().execute('admin', 'admin')
+  if (!(loginResult instanceof Error)) return ok({ id: loginResult.userAccount.id })
+
+  return err(registerResult)
+}
+
+async function assignAllRolesToAccount(authorization: AuthorizationApplication, userId: number) {
+  const roles = ['default', 'genre-editor', 'admin']
+  for (const role of roles) {
+    const result = await authorization.assignRoleToUser(
+      userId,
+      role,
+      authorization.getSystemUserId(),
+    )
+    if (result.isErr()) return result
+  }
+  return ok(undefined)
 }
