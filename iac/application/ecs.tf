93f5892d7bf5eb8611fa569becb65f8272eef25d
Jake Cheek <jake@f-m.fm>
2025-03-25 16:59:43 -0400

    ops: deploy 2 instances of frontend and backend

--- iac/application/ecs.tf
+++ iac/application/ecs.tf
@@ -143,7 +143,7 @@ resource "aws_ecs_service" "frontend" {
   name            = "frontend-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.frontend.arn
-  desired_count   = 1
+  desired_count   = 2
   launch_type     = "FARGATE"
 
   # This forces a new deployment when task definition changes
@@ -167,7 +167,7 @@ resource "aws_ecs_service" "backend" {
   name            = "backend-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.backend.arn
-  desired_count   = 1
+  desired_count   = 2
   launch_type     = "FARGATE"
 
   # This forces a new deployment when task definition changes
