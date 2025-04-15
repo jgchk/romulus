15668d682baf7ee4b601b5f437d5301c9e65b1f9
Jake Cheek <jake@f-m.fm>
2025-04-15 06:22:50 -0400

    refactor: move navbar to component

---
+++ apps/frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import Card from '$lib/atoms/Card.svelte'
+  import { getUserContext } from '$lib/contexts/user'
+
+  import AccountDropdown from '../../routes/AccountDropdown.svelte'
+
+  const user = getUserContext()
+</script>
+
+<nav class="flex justify-between">
+  <Card
+    class="flex p-1 text-sm font-bold tracking-wide text-gray-600 transition dark:text-gray-300"
+  >
+    <a
+      href="/genres"
+      class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+      >Tree</a
+    >
+    <a
+      href="/genres/table"
+      class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+      >Table</a
+    >
+    <a
+      href="/genres/latest"
+      class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+      >Latest</a
+    >
+    <a
+      href="/genres/random"
+      class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+      >Random</a
+    >
+  </Card>
+
+  <Card
+    class="flex gap-1 p-1 text-sm font-bold tracking-wide text-gray-600 transition dark:text-gray-300"
+  >
+    {#if $user}
+      <AccountDropdown account={$user} />
+    {:else}
+      <a
+        href="/sign-in"
+        class="h-full rounded border border-transparent bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+        >Sign in</a
+      >
+      <a
+        href="/sign-up"
+        class="h-full rounded border border-gray-700 border-opacity-25 bg-gray-800 px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-700"
+        >Sign up</a
+      >
+    {/if}
+  </Card>
+</nav>
--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -5,14 +5,13 @@
   import { writable } from 'svelte/store'
 
   import { browser } from '$app/environment'
-  import Card from '$lib/atoms/Card.svelte'
   import Toaster from '$lib/atoms/Toast/Toaster.svelte'
+  import Navbar from '$lib/components/Navbar.svelte'
   import { setUserContext, type UserStore } from '$lib/contexts/user'
   import { setUserSettingsContext } from '$lib/contexts/user-settings'
   import UserSettingsStore from '$lib/contexts/user-settings/store'
 
   import type { LayoutData } from './$types'
-  import AccountDropdown from './AccountDropdown.svelte'
   import DarkModeApplier from './DarkModeApplier.svelte'
 
   type Props = {
@@ -54,51 +53,7 @@
   <div
     class="flex h-full w-full flex-col gap-1 bg-gray-200 p-2 text-black transition dark:bg-gray-800 dark:text-white"
   >
-    <nav class="flex justify-between">
-      <Card
-        class="flex p-1 text-sm font-bold tracking-wide text-gray-600 transition dark:text-gray-300"
-      >
-        <a
-          href="/genres"
-          class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
-          >Tree</a
-        >
-        <a
-          href="/genres/table"
-          class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
-          >Table</a
-        >
-        <a
-          href="/genres/latest"
-          class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
-          >Latest</a
-        >
-        <a
-          href="/genres/random"
-          class="h-full rounded bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
-          >Random</a
-        >
-      </Card>
-
-      <Card
-        class="flex gap-1 p-1 text-sm font-bold tracking-wide text-gray-600 transition dark:text-gray-300"
-      >
-        {#if data.user}
-          <AccountDropdown account={data.user} />
-        {:else}
-          <a
-            href="/sign-in"
-            class="h-full rounded border border-transparent bg-transparent px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
-            >Sign in</a
-          >
-          <a
-            href="/sign-up"
-            class="h-full rounded border border-gray-700 border-opacity-25 bg-gray-800 px-2.5 py-1 transition hover:bg-gray-200 dark:hover:bg-gray-700"
-            >Sign up</a
-          >
-        {/if}
-      </Card>
-    </nav>
+    <Navbar />
     <main class="flex-1 overflow-auto">
       {@render children?.()}
     </main>
