b7169a2db5e574db30e2af2a132fc2febaf2bb9f
Jake Cheek <jake@f-m.fm>
2025-04-15 08:07:29 -0400

    feat: enable deleting media types

---
+++ apps/frontend/src/routes/media/types/[id]/delete/+page.server.ts
@@ -0,0 +1,37 @@
+import { error, redirect } from '@sveltejs/kit'
+
+import { routes } from '$lib/routes'
+
+import type { Actions } from './$types'
+
+export const actions = {
+  default: async ({ request, locals }: { request: Request; locals: App.Locals }) => {
+    const data = await request.formData()
+    const id = data.get('id')
+
+    if (id === null) {
+      return error(400, { message: 'Missing id' })
+    }
+    if (id instanceof File) {
+      return error(400, { message: 'Invalid id' })
+    }
+
+    const result = await locals.di.media().deleteMediaType(id)
+    if (result.isErr()) {
+      switch (result.error.name) {
+        case 'FetchError': {
+          return error(500, result.error.message)
+        }
+        case 'UnauthenticatedError':
+        case 'UnauthorizedError': {
+          return error(result.error.statusCode, result.error.message)
+        }
+        default: {
+          result.error satisfies never
+        }
+      }
+    }
+
+    return redirect(303, routes.media.types.route())
+  },
+} satisfies Actions
