37064ab7c859c606832f8df1ca31cdad38e83046
Jake Cheek <jake@f-m.fm>
2025-04-17 17:36:12 -0400

    test: increase test timeout

--- services/authentication/vitest.config.ts
+++ services/authentication/vitest.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     include: ['./src/**/*.test.ts'],
     setupFiles: ['./src/vitest-setup.ts'],
     fileParallelism: false,
-    testTimeout: 10000, // FIXME: This is a patch for long-running tests that rely on PGlite. We should remove reliance on PGlite so we can remove this.
+    testTimeout: 15000, // FIXME: This is a patch for long-running tests that rely on PGlite. We should remove reliance on PGlite so we can remove this.
   },
 })
